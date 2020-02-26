1f7261f504e88ba255c15e02b051ea0290a92976
MarcelSin <sys920525@gmail.com>
2020-02-27 07:40:16 +0900

    Title : IO & HashMap, version 1.0

---
+++ BOJ-10809/Main.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		IO_Manager io = new IO_Manager();
+		HashMap<Character, Integer> hm = new HashMap<Character,Integer>();
+		for(char c = 'a'; c < 'z'+1; c++) hm.put(c, -1);
+		
+		String str = io.inputStr();
+		
+		for (int i = 0; i < str.length(); i++) {
+			char ascii = str.charAt(i);
+			if (hm.get(ascii) != -1) continue;
+			hm.put(ascii,i);
+		}
+		
+		for(char c = 'a'; c < 'z'+1; c++) io.write(hm.get(c)+" ");
+		io.close();
+		
+	}		
+	static public int nextTokenInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	
+}
+
+
+
+
+
+
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	private BufferedReader br;
+	private BufferedWriter bw;
+	
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
