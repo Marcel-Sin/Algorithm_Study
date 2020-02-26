0e3e0d120bb6fbe0e7b8d79fbbfbb96de6a2fb88
MarcelSin <sys920525@gmail.com>
2020-02-27 08:47:00 +0900

    Title : chars length, version 1.0

---
+++ BOJ-2743/Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		IO_Manager io = new IO_Manager();
+		// if not use The Length method or so. 
+		int len = 0;
+		Integer c = 0;
+		while(c != '\n') {
+			c = System.in.read();
+			if (c != '\n') len++;
+		}
+		io.write((len-1)+"");
+		io.close();
+		
+		
+		
+		
+		
+		
+		
+		
+	}		
+	static public int nextTokenInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	
+}
+
+
+
+
+
+
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+	
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
