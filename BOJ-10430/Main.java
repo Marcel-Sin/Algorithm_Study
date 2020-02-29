89a6c8af7b4a091dd6ed946b83a9b87fd7b3205f
MarcelSin <sys920525@gmail.com>
2020-02-29 09:42:55 +0900

    Title : mod number, version 1.0

---
+++ BOJ-10430/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		IO_Manager io = new IO_Manager();
+		StringTokenizer stk = new StringTokenizer(io.inputStr()); 
+		int a = nextTokenInt(stk);
+		int b = nextTokenInt(stk);
+		int c = nextTokenInt(stk);
+		io.write((a+b)%c+"\n");
+		io.write((a%c+b%c)%c+"\n");
+		io.write((a*b)%c+"\n");
+		io.write((a%c * b%c)%c+"");
+		io.close();
+		
+	}
+		
+	static public int nextTokenInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+
+}
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+	
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
