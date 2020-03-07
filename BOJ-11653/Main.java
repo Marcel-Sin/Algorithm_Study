72f6cb60905d9fffe051db989df38ae8ef880f0e
MarcelSin <sys920525@gmail.com>
2020-03-07 15:18:24 +0900

    Title : Factorization, version 1.0

---
+++ BOJ-11653/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		IO_Manager io = new IO_Manager();
+		int number = io.inputInt();
+		int value = number;
+		while(value != 1) {
+			for(int x = 2; x <= value; x++) {
+				if(value % x == 0) {
+					value = value/x;
+					io.write(x+"\n");
+					break;
+				}
+			}
+		}
+		if(number == 1) io.write(1+"\n");
+		io.close();
+	}
+
+	
+
+	static public int nextTokenInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+
+}
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+	
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
