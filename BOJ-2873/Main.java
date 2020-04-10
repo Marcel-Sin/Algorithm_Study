e44a65b2c056c06da0eb069cd95df50d09620123
MarcelSin <sys920525@gmail.com>
2020-04-10 21:44:14 +0900

    Title : Greedy & implementation, version 3.5

--- BOJ-2873/Main.java
+++ BOJ-2873/Main.java
@@ -10,18 +10,14 @@ import java.util.StringTokenizer;
 public class Main {
 	static IO_Manager io = new IO_Manager();
 	static int Row=0,Col=0;
-	static boolean collision = false;
 	static int[][] map;
-	static int[][] check;
-	static int lastRow, lastCol;
-	static char[] AI = new char[4];
-	static boolean UP_arise = false;
-	static boolean zigzag = false;
+	static boolean[][] check;
+	static int lastRow, lastCol,minRow,minCol,zigzagStartRow;
 	public static void main(String[] args) throws IOException {
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
 		int r = nextInt(stk),c=nextInt(stk);
 		map = new int[r][c];
-		check = new int[r][c];
+		check = new boolean[r][c];
 		lastRow = r-1;
 		lastCol = c-1;
 		for(int i = 0; i < r; i++) {
@@ -29,136 +25,120 @@ public class Main {
 			for(int j = 0; j < c; j++) map[i][j] = nextInt(stk);
 		}
 		
-		if(r%2 == 1) {
+		if (r % 2 == 1)
+			Odd_Horizontal();
+		else if (c % 2 == 1)
+			Odd_Vertical();
+		else if (r % 2 == 0 && c % 2 == 0) {
+			Finding_Minimum();
+			zigzagStartRow = (minRow/2)*2;
+			Row = 0;
+			Col = 0;
 			char dir = 'R';
-			for(int i = 0; i < r; i++) {
-				for(int j = 0; j < c-1; j++) {
-					io.bw.write(dir);
-				}
-				if(i != r-1) io.bw.write('D');
-				if(dir == 'L') dir = 'R';
-				else if(dir == 'R') dir = 'L';
+			for(int i = 0; i < zigzagStartRow; i++) {
+				LineMove(dir,lastCol);
+				dir = (dir == 'R')? 'L':'R';
+				io.bw.write('D');
+				Row=i+1;
+			} 
+			check[Row][Col] = true;
+			check[minRow][minCol] = true;
+			ZigzagBarrier(zigzagStartRow-1,zigzagStartRow+2);
+			int zigloop = (lastCol+1)*2-1;
+			for(int i=0; i < zigloop; i++) {
+				boolean checker = ZigzagUP();
+				if(checker == false) checker = ZigzagDOWN();
+				if(checker == false) checker = ZigzagRIGHT();
 			}
-		}
-		else if(c%2 == 1) {
-			char dir = 'D';
-			for(int i = 0; i < c; i++) {
-				for(int j = 0; j < r-1; j++) {
-					io.bw.write(dir);
-				}
-				if(i != c-1) io.bw.write('R');
-				if(dir == 'D') dir = 'U';
-				else if(dir == 'U') dir = 'D';
-			}
-		}
-		else if(r%2 == 0 && c%2 == 0){
-			int min = 10000;
-			int y=0,x=0;
-			for(int i=0; i < r; i++) {
-				for(int j=0; j < c; j++) {
-					if(i+j % 2 == 0) continue;
-					if(map[i][j] < min) {
-						min = map[i][j];
-						x = j;
-						y = i;
-					}
+			if(Row != lastRow) {
+				io.bw.write('D');
+				int finalLoop = lastRow-Row;
+				dir = 'L';
+				for(int i = 0; i < finalLoop; i++) {
+					LineMove(dir,lastCol);
+					dir = (dir == 'R')? 'L':'R';
+					if(i != finalLoop-1) io.bw.write('D');
 				}
 			}
-			check[0][0] = 1;
-			check[y][x] = 99999999;
-			if(y >= lastRow-1) zigzag = true;
-			Priority_Change('R','L','D','U');
-			
-			while (Row != lastRow || Col != lastCol) {
-				int value = Priority_Movement();
-				if(value == 99999999) { 
-					Move('D');
-					Priority_Change(AI[3],AI[0],AI[2],AI[1]);
-					collision = true;
-				}
-			}
-		}
+		}// Even if End
 		io.close();
 	}
-		
-		
-		
 	
 	
-	
-	static int Move(char dir) throws IOException{
-		if(dir == 'D' && Row+1 <= lastRow) {
-			if(check[Row+1][Col] == 0) {
-				Row++;
-				check[Row][Col] = 1;
-				io.bw.write('D');
-				if(collision && UP_arise == true) {
-					Priority_Change(AI[2], AI[1], AI[0], AI[3]);
-					UP_arise = false;
-				}
-				else if(zigzag && Row >= lastRow-1) Priority_Change('U','D','R','L');	
-				else if(collision && Col == lastCol) Priority_Change(AI[0], 'L', AI[2], 'R');
-				else if(collision && Col == 0) Priority_Change(AI[0], 'R', AI[2], 'L');
-				else if(!collision && Col == lastCol) Priority_Change('L', 'R',AI[2],AI[3]);
-				else if(!collision && Col == 0) Priority_Change('R', 'L',AI[2],AI[3]);
-				return 1;
-			}
-			else if (check[Row+1][Col] == 1) return 0;
+	static void Odd_Horizontal() throws IOException{
+		char dir = 'R';
+		for(int i = 0; i <= lastRow; i++) {
+			for(int j = 0; j < lastCol; j++) io.bw.write(dir);
+			if(i != lastRow) io.bw.write('D');
+			dir = (dir == 'R')? 'L':'R';
 		}
-		
-		else if(dir == 'U' && Row-1 >= 0) {
-			if(check[Row-1][Col] == 0) {
-				Row--;
-				check[Row][Col] = 1;
-				io.bw.write('U');
-				if (zigzag == false) {
-					UP_arise = true;
-					Priority_Change(AI[2], AI[1], AI[0], AI[3]);
-				}
-				return 1;
-			}
-			else if (check[Row-1][Col] == 1) {
-				return 0;
-			}
-		}
-		else if(dir == 'R' && Col+1 <= lastCol) {
-			if (check[Row][Col+1] == 0) {
-				Col++;
-				check[Row][Col] = 1;
-				io.bw.write('R');
-				return 1; 
-			}
-			else if (check[Row][Col+1] == 1) return 0;
-			else return 99999999;
-		}
-		else if(dir == 'L' && Col-1 >= 0) {
-			if (check[Row][Col-1] == 0) {
-				Col--;
-				check[Row][Col] = 1;
-				io.bw.write('L');
-				return 1;
-			}
-			else if (check[Row][Col-1] == 1) return 0;
-			else return 99999999;
-		}
-		return 0;
 	}
-	
-	static int Priority_Movement() throws IOException{
-		int checker = -1;
-		checker = Move(AI[0]);
-		if(checker == 0) checker = Move(AI[1]);
-		if(checker == 0) checker = Move(AI[2]);
-		if(checker == 0) checker = Move(AI[3]);
-		return checker;
+	static void Odd_Vertical() throws IOException{
+		char dir = 'D';
+		for(int i = 0; i <= lastCol; i++) {
+			for(int j = 0; j < lastRow; j++) io.bw.write(dir);
+			if(i != lastCol) io.bw.write('R');
+			dir = (dir == 'U')? 'D':'U';
+		}
 	}
 
-	static void Priority_Change(char a, char b, char c, char d) {
-		AI[0] = a;
-		AI[1] = b;
-		AI[2] = c;
-		AI[3] = d;
+	static void Finding_Minimum() {
+		int min = 10000;
+		for (int i = 0; i <= lastRow; i++) {
+			int startCol = (i%2 == 1)? 0:1;
+			for (int j = startCol; j <= lastCol; j+=2) {
+				if (map[i][j] < min) {
+					min = map[i][j];
+					minRow = i;
+					minCol = j;
+				}
+			}
+		}
 	}
+	static void LineMove(char dir, int moveCount) throws IOException{
+		for(int j = 0; j < moveCount; j++) io.bw.write(dir);
+	}
+	static void	ZigzagBarrier(int startRow, int endRow) {
+		if(startRow >= 0) {
+			for(int i = 0; i <= lastCol; i++) {
+				check[startRow][i] = true;
+			}
+		}
+		if(endRow <= lastRow) {
+			for(int i = 0; i <= lastCol; i++) {
+				check[endRow][i] = true;
+			}
+		}
+	}
+	static boolean ZigzagUP() throws IOException{
+		if(Row-1 >= 0 && check[Row-1][Col] == false) {
+			Row--;
+			check[Row][Col] = true;
+			io.bw.write('U');
+			return true;
+		}
+		return false;
+	}
+	static boolean ZigzagDOWN() throws IOException{
+		if(Row+1 <= lastRow  && check[Row+1][Col] == false) {
+			Row++;
+			check[Row][Col] = true;
+			io.bw.write('D');
+			return true;
+		}
+		return false;
+	}
+	static boolean ZigzagRIGHT() throws IOException{
+		if(Col+1 <= lastCol  && check[Row][Col+1] == false) {
+			Col++;
+			check[Row][Col] = true;
+			io.bw.write('R');
+			return true;
+		}
+		return false;
+	}
+	
+	
 	
 	
 	static int nextInt(StringTokenizer stk) {
