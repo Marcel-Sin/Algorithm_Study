5bbb7fbc1cdabc405fd8063cbc29dcad093927ad
MarcelSin <sys920525@gmail.com>
2020-05-03 13:09:33 +0900

    Title : Brute Force version 1.0 (Recursive)

---
+++ BOJ-2003/Main.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static int SIZE,TARGET,COUNT = 0;
+	static int[] A;
+	public static void main(String[] args) throws IOException {
+		Initializing();
+		for(int i = 0; i < SIZE; i++) {
+			DFS(i,A[i]);
+		}
+		System.out.println(COUNT);
+
+	}
+	
+	static void DFS(int idx,int sum) {
+		if(sum == TARGET) {
+			COUNT++;
+			return;
+		}
+		else if (TARGET < sum) {
+			return;
+		}
+		
+		if(idx+1 < SIZE) DFS(idx+1,sum+A[idx+1]);
+	}
+	
+
+	
+	
+	static void Initializing() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		SIZE = nextInt(stk);
+		TARGET = nextInt(stk);
+		stk = new StringTokenizer(io.inputStr());
+		A = new int[SIZE];
+		for(int i = 0; i < SIZE; i++) A[i] = nextInt(stk);
+	}
+
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static void Swap(int[] a, int i , int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] =temp;
+	}	
+	static void PartialReverse(int[] a, int start, int end) {
+		int temp;
+		while(end > start) {
+			temp = a[start];
+			a[start++] = a[end];
+			a[end--] = temp;
+		}
+	}
+}
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
