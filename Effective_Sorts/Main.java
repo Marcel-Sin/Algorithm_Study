75db154b97a8acfe40e52bfd268a572dcad605a3
MarcelSin <sys920525@gmail.com>
2020-05-13 13:46:48 +0900

    InsertSort, QuickSort, MergeSort Practice

--- Effective_Sorts/Main.java
+++ Effective_Sorts/Main.java
@@ -8,89 +8,90 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int[] MEM = new int[10];
+	static long startTime,endTime;
+	static int[] A,B,C;
+	static int[] TEMP = new int[100000];
 	public static void main(String[] args) throws IOException {
-		int[] t = {8,3,2,7,9,1,2,0,0,0};
-		//InsertSort(t,7);
-		//QuickSort(t,0,6);
-		//MergeSort(t,0,6,MEM);
-		for(int n : t) System.out.print(n + " ");
+		Init();
+		startTime = System.currentTimeMillis();
+		InsertionSort(A);
+		endTime = System.currentTimeMillis();
+		System.out.println("InsertionSort Time(ms) : "+(endTime-startTime));
+		
+		startTime = System.currentTimeMillis();
+		MergeSort(B, 0, B.length-1, TEMP);
+		endTime = System.currentTimeMillis();
+		System.out.println("MergeSort Time(ms) : "+(endTime-startTime));
+		
+		startTime = System.currentTimeMillis();
+		QuickSort(C, 0, C.length-1);
+		endTime = System.currentTimeMillis();
+		System.out.println("QuickSort Time(ms) : "+(endTime-startTime));
 	}
 
-	static int Partition(int[] a, int first, int last, int pivot) {
-		int SI = first,P = a[pivot];
-		// Pivot to Last
-		Swap(a,pivot,last);
-		
-		// Make Groups
-		for(int i = first; i < last; i++) {
-			if(a[i] < P) Swap(a,i,SI++);	
-		}
-		
-		// Pivot Positioning
-		Swap(a,SI,last);
-		return SI;
+	static void Insertion(int[] a, int size, int input) {
+		int i = 0;
+		while(i < size && a[i] < input) i++;
+		for(int j = size; j > i; j--) a[j] = a[j-1];	
+		a[i] = input;
 	}
-	
-	static void QuickSort(int[] a, int first, int last) {
-		//Basic Condition
-		if(last <= first) return;
-		
-		//Divide
-		int part = Partition(a, first, last, (first+last)/2);
-		
-		//Conquer
-		QuickSort(a,first,part);
-		QuickSort(a,part+1,last);
-	}
-	static void Swap(int[] a,int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-	
-	static void Insertion(int[] a, int size, int num) {
-		int i = 0,temp = num;
-		// Find
-		while(i < size && a[i] < num) i++;
-		
-		// Push
-		for(int j = size; j > i; j--) a[j] = a[j-1];
-		
-		// Input
-		a[i] = num;
-	}
-	static void InsertSort(int[] a, int size) {
-		if(size == 1) return;
+	static void InsertionSort(int[] a) {
+		int size = a.length;
 		for(int i = 0; i < size; i++) {
 			Insertion(a,i,a[i]);
 		}
 	}
 	
-	static void MergeSort(int[] a, int low, int high, int[] temp) {
-		//Basic Condition
+	static void MergeSort(int[] a, int low, int high, int[] mem) {
 		if(high <= low) return;
 		
-		//Divide
-		int mid = (low + high)/2;
+		int mid = (low+high)/2;
 		
-		//Conquer
-		MergeSort(a,low,mid,temp);
-		MergeSort(a,mid+1,high,temp);
+		MergeSort(a,low,mid,mem);
+		MergeSort(a,mid+1,high,mem);
 		
-		//Combine
 		int i = low, j = mid+1;
 		for(int k = low; k <= high; k++) {
-			if(mid < i) temp[k] = a[j++];
-			else if(high < j) temp[k] = a[i++];
-			else if(a[i] > a[j]) temp[k] = a[j++];
-			else temp[k] = a[i++];
+			if(i > mid) mem[k] = a[j++];
+			else if(j > high) mem[k] = a[i++];
+			else if(a[i] > a[j]) mem[k] = a[j++];
+			else mem[k] = a[i++];
 		}
 		
-		//Copy
-		for(i = low; i <= high; i++) a[i] = temp[i];	
+		for(i = low; i <= high; i++) a[i] = mem[i];	
 	}
 	
+	static int Partition(int[] a, int low, int high, int pivot) {
+		int P = a[pivot], SI = low;
+		Swap(a,pivot,high);
+		for(int i = low; i < high; i++) {
+			if(a[i] < P) Swap(a,i,SI++);
+		}
+		Swap(a,SI,pivot);
+		return SI;
+	}
+	static void QuickSort(int[] a, int low, int high) {
+		if(low >= high) return;
+		int part = Partition(a, low, high, (low+high)/2);
+		QuickSort(a, low, part-1);
+		QuickSort(a, part+1, high);
+	}
+	static void Swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	static void Init() {
+		A = new int[100000];
+		for(int i = 0; i < 100000; i++) A[i] = Random(0,100000);
+		B = A.clone();
+		C = A.clone();
+	}
+	static int Random(int min,int max) {
+		int number = (int)(Math.random()*max);
+		return number;
+	}
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
