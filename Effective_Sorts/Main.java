a491e37c7304c0ae61097f2cd47171711e3ac113
MarcelSin <sys920525@gmail.com>
2020-05-13 16:13:16 +0900

    QuickSort,MergeSort Recur,notRecur Compare

--- Effective_Sorts/Main.java
+++ Effective_Sorts/Main.java
@@ -3,30 +3,38 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
 	static long startTime,endTime;
-	static int[] A,B,C;
-	static int[] TEMP = new int[100000];
+	static int[] A,B,C,D;
+	static int[] TEMP = new int[10000000];
 	public static void main(String[] args) throws IOException {
 		Init();
 		startTime = System.currentTimeMillis();
-		InsertionSort(A);
+		QuickSort(A,0,A.length-1);
 		endTime = System.currentTimeMillis();
-		System.out.println("InsertionSort Time(ms) : "+(endTime-startTime));
-		
+		System.out.println("QuickSort(Recur) Time(ms) : "+(endTime-startTime));
+
 		startTime = System.currentTimeMillis();
-		MergeSort(B, 0, B.length-1, TEMP);
+		QuickSort_NotRecursive(B);
 		endTime = System.currentTimeMillis();
-		System.out.println("MergeSort Time(ms) : "+(endTime-startTime));
+		System.out.println("QuickSort(NotRecur) Time(ms) : "+(endTime-startTime));
 		
+
 		startTime = System.currentTimeMillis();
-		QuickSort(C, 0, C.length-1);
+		MergeSort(C,0,C.length-1,TEMP);
 		endTime = System.currentTimeMillis();
-		System.out.println("QuickSort Time(ms) : "+(endTime-startTime));
+		System.out.println("MergeSort(Recur) Time(ms) : "+(endTime-startTime));
+
+		startTime = System.currentTimeMillis();
+		MergeSort_NotRecursive(D);
+		endTime = System.currentTimeMillis();
+		System.out.println("MergeSort(NotRecur) Time(ms) : "+(endTime-startTime));
+		
 	}
 
 	static void Insertion(int[] a, int size, int input) {
@@ -46,7 +54,6 @@ public class Main {
 		if(high <= low) return;
 		
 		int mid = (low+high)/2;
-		
 		MergeSort(a,low,mid,mem);
 		MergeSort(a,mid+1,high,mem);
 		
@@ -67,7 +74,7 @@ public class Main {
 		for(int i = low; i < high; i++) {
 			if(a[i] < P) Swap(a,i,SI++);
 		}
-		Swap(a,SI,pivot);
+		Swap(a,SI,high);
 		return SI;
 	}
 	static void QuickSort(int[] a, int low, int high) {
@@ -82,11 +89,68 @@ public class Main {
 		a[j] = temp;
 	}
 
+	static void QuickSort_NotRecursive(int[] a) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(a.length-1);
+		stack.push(0);
+		int low, high, part;
+		int s,e,pivot,P,SI;
+		while(stack.isEmpty() == false) {
+			low = stack.pop();
+			high = stack.pop();
+			if(high <= low) continue;
+			//Partition
+			pivot = (low+high)/2;
+			P = a[pivot]; SI = low;
+			Swap(a,pivot,high);
+			for(int i = low; i < high; i++) {
+				if(a[i] <= P) Swap(a,i,SI++);
+			}
+			Swap(a,SI,high);
+			part = SI;
+			
+			stack.push(part-1);
+			stack.push(low);
+		
+			stack.push(high);
+			stack.push(part+1);
+		}
+	}
+	static void MergeSort_NotRecursive(int[] a) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(a.length-1);
+		stack.push(0);
+		int low,high,mid;
+		int i,j;
+		while(stack.isEmpty() == false) {
+			low = stack.pop();
+			high = stack.pop();
+			if(low >= high) continue;
+			mid = (low+high)/2;
+			stack.push(mid);
+			stack.push(low);
+			
+			stack.push(high);
+			stack.push(mid+1);
+			
+			i = low; j = mid+1;
+			for(int k = low; j <= high; k++) {
+				if(i > mid) TEMP[k] = a[j++];
+				else if(high < j) TEMP[k] = a[i++];
+				else if(a[i] > a[j]) TEMP[k] = a[j++];
+				else TEMP[k] = a[i++];
+			}
+			
+			for(i = low; i <= high; i++) a[i] = TEMP[i];
+		}
+	}
+	
 	static void Init() {
-		A = new int[100000];
-		for(int i = 0; i < 100000; i++) A[i] = Random(0,100000);
+		A = new int[10000000];
+		for(int i = 0; i < 10000000; i++) A[i] = Random(0,10000000);
 		B = A.clone();
 		C = A.clone();
+		D = A.clone();
 	}
 	static int Random(int min,int max) {
 		int number = (int)(Math.random()*max);
