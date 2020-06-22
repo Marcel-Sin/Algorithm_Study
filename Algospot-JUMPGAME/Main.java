74f6867d68c787ee7406c0bce4dbfe32fb834c70
MarcelSin <sys920525@gmail.com>
2020-06-22 15:15:23 +0900

    Title : JUMPGAME, using BruteForce

---
+++ Algospot-JUMPGAME/Main.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static int TOTAL;
+	static int[][] MAP;	
+	static int SIZE;
+	static boolean ANS;
+	public static void main(String[] args) throws IOException {
+		TOTAL = io.inputInt();
+		for(int i = 0; i < TOTAL; i++) {
+			Init();
+			DFS(0,0);
+			if(ANS) System.out.println("YES");
+			else System.out.println("NO");
+		}
+	}   
+		
+	static void DFS(int curR, int curC) {
+		if(MAP[curR][curC] == 0) {
+			ANS = true;
+			return;
+		}
+		int walkCount = MAP[curR][curC];
+		if(curR+walkCount < SIZE) DFS(curR+walkCount,curC);
+		if(curC+walkCount < SIZE) DFS(curR,curC+walkCount);
+		return;
+	}
+	
+	
+	
+	
+	static void Init() throws IOException{
+		SIZE = io.inputInt();	
+		MAP = new int[SIZE][SIZE];
+		for(int a = 0; a < SIZE; a++) {
+			StringTokenizer stk = new StringTokenizer(io.inputStr());
+			for (int b = 0; stk.hasMoreTokens(); b++) MAP[a][b] = nextInt(stk);
+		}
+		ANS = false;
+	}
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static int Min(int a, int b) {
+		return (a > b)?b:a;
+	}
+	static int Max(int a, int b) {
+		return (a > b)?a:b;
+	}
+}
+
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}}
