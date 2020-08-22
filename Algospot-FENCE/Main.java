7c0b070a3fbabb8d660df9ba1d74779a5cfb6e82
MarcelSin <sys920525@gmail.com>
2020-08-22 15:38:55 +0900

    Title : Fence Solution using Sweeping

--- Algospot-FENCE/Main.java
+++ Algospot-FENCE/Main.java
@@ -4,85 +4,89 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int TOTAL;
-	static int[] WOOD;
-	static int SIZE;
-	static int ANS;
-	
+	static int TOTAL, N;
+	static int[] WOOD = new int[20001];
 	public static void main(String[] args) throws IOException {
 		TOTAL = io.inputInt();
-		for(int i = 0; i < TOTAL; i++) {
+		for (int i = 0; i < TOTAL; i++) {
 			Init();
-			System.out.println(DNC_Solve(0,SIZE-1));
+			System.out.println(Max_Fence());
 		}
 	}
-	
-	static int DNC_Solve(int left, int right) {
-		
-		//Base Condition
-		if (left >= right) return WOOD[left];
-		
-		//Divide
-		int mid = (left+right)/2;
-		
-		//conquer
-		int resultA = Max(DNC_Solve(left,mid),DNC_Solve(mid+1,right));
-		int resultB = Max(resultA, Middle_Max(mid,left,right));
-		
-		return resultB;
-	}
-	
-	static int Middle_Max(int mid, int leftBound, int rightBound) {
-		if (leftBound >= rightBound)
-			return WOOD[leftBound];
-		int size,max, l = mid, r = mid + 1, height;
-		height = Min(WOOD[l], WOOD[r]);
-		max = height * 2;
-		
-		while (leftBound < l && r < rightBound) {
-			if (WOOD[l - 1] < WOOD[r + 1]) height = Min(height, WOOD[++r]);
-			else height = Min(height, WOOD[--l]);
-			
-			size = height * (r - l + 1);
-			max = Max(size, max);
-		}
-		while (r < rightBound) {
-			height = Min(height, WOOD[++r]);
-			size = height * (r - l + 1);
-			max = Max(size, max);
-		}
-		while (leftBound < l) {
-			height = Min(height, WOOD[--l]);
-			size = height * (r - l + 1);
-			max = Max(size, max);
-		}
-		return max;	
-	}
-	
-	static int Min(int a, int b) {
-		return (a > b)?b:a;
-	}
-	static int Max(int a, int b) {
-		return (a > b)?a:b;
-	}
-	
-	static void Init() throws IOException{
-		SIZE = io.inputInt();
+
+
+	static void Init() throws IOException {
+		N = io.inputInt();
+		Arrays.fill(WOOD, 0);
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		WOOD = new int[SIZE];
-		for(int i = 0; i < SIZE; i++) WOOD[i] = nextInt(stk);
+		for(int i = 0; stk.hasMoreTokens(); i++) WOOD[i] = nextInt(stk);
 	}
+	
+	static int Max_Fence() {
+		Stack<Integer> stack = new Stack<Integer>();
+		int ret = 0;
+		int right_idx = 0, left_idx = 0, width = 0, square = 0;
+		
+		for(int i = 0; i <= N; i++) {
+			// 이번 차례에 i번째 판자가 Stack.Top() 보다 작다는 건, 스택 탑의 경계점(right)가 i번째 판자
+			while(!stack.isEmpty() && WOOD[stack.peek()] >= WOOD[i] ) {
+				int current_height = WOOD[stack.pop()];
+				
+				right_idx = i;
+				
+				if(stack.isEmpty()) left_idx = -1;
+				else left_idx = stack.peek();
+				
+				width = right_idx - left_idx - 1;
+				
+				square = current_height * width;
+				ret = Max(ret,square);
+			}
+			stack.push(i);
+		}
+		return ret;
+	}
+	
+	
+	
+	// ===================== functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -110,4 +114,5 @@ class IO_Manager {
 	public void close() throws IOException {
 		br.close();
 		bw.close();
-	}}
+	}
+}
