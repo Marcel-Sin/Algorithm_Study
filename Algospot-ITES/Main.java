ab14b092ae794b93bac6a981c4895db9f40f4297
MarcelSin <sys920525@gmail.com>
2020-08-26 12:51:48 +0900

    Title : Large Size Case Counting using My_Queue

---
+++ Algospot-ITES/Main.java
@@ -0,0 +1,184 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static int TOTAL;
+	static long SIGNAL;
+	static int SIGNAL_NUMBER;
+	static long DIVIDER = 2l << 31l;
+	static int K,N;
+   static My_Queue QUEUE = new My_Queue(5000001);
+	
+	public static void main(String[] args) throws IOException {
+		TOTAL = io.inputInt();
+		for (int i = 0; i < TOTAL; i++) {
+			Init();
+			long start = System.currentTimeMillis();
+			int ans = Solve();
+			long end = System.currentTimeMillis();
+			System.out.printf("%d\n",ans);
+		}
+	}
+
+	static int Solve() {
+		
+		int sum = 0, count = 0, num = 0;
+		QUEUE.Enqueue(1983);
+		sum = 1983;
+		while(true) {
+			if(sum == K) count++;
+			
+			if(SIGNAL_NUMBER < N && sum <= K) {
+				num = Next_Signal();
+				QUEUE.Enqueue(num);
+				sum += num;
+			}
+			else if(sum > K){
+				num = QUEUE.Dequeue();
+				sum -= num;
+			}
+			
+			if(SIGNAL_NUMBER == N && sum < K) {
+				break;
+			}
+		}
+		return count;
+	}
+	
+	
+
+	static int Next_Signal() {
+		SIGNAL_NUMBER++;
+		SIGNAL = (SIGNAL * 214013 + 2531011) % DIVIDER;
+		return (int)(SIGNAL % 10000 + 1);
+	}
+	
+	static void Init() throws IOException {
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		K = nextInt(stk);
+		N = nextInt(stk);
+		
+		SIGNAL = 1983;
+		SIGNAL_NUMBER = 1;
+		QUEUE.Clean();
+	}
+	
+	
+	static class My_Queue {
+		int head, tail, count, size;
+		int[] arr;
+
+		public My_Queue(int size) {
+			super();
+			this.head = 0;
+			this.tail = 0;
+			this.count = 0;
+			this.size = size;
+			this.arr = new int[size];
+		}
+		public void Enqueue(int n) {
+			if (count + 1 > size)
+				return;
+			arr[tail] = n;
+			tail = (tail + 1) % size;
+			count++;
+			return;
+		}
+		
+		public int Dequeue() {
+			if (count == 0)
+				return -1;
+			int ret = arr[head];
+			head = (head + 1) % size;
+			if (count == 1)
+				tail = head;
+			count--;
+			return ret;
+		}
+		public int Peek() {
+			if (count == 0)
+				return -1;
+			return arr[head];
+		}
+		public int Count() {
+			return count;
+		}
+		
+		public boolean isEmpty() {
+			return (count == 0);
+		}
+		
+		public void Clean() {
+			tail = head;
+			count = 0;
+		}
+	}
+	
+	
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
