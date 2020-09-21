701f85500351111ea8088c632140d55815894e3a
MarcelSin <sys920525@gmail.com>
2020-09-21 23:18:19 +0900

    Title : making Treap using the AVL-Rotation

--- Treap_using_AVL_Rotate/Main.java
+++ Treap_using_AVL_Rotate/Main.java
@@ -8,10 +8,36 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-
+	static BST tree;
 	public static void main(String[] args) throws IOException {
+		tree = new BST(0);
+		tree.priority = Integer.MIN_VALUE;
+		
+		tree.Insert(5);
+		tree.Insert(3);
+		tree.Insert(6);
+		tree.Insert(9);
+		tree.Insert(-99);
+		
+		
+		Travel_MLR(tree.left);
 		
 	}
+	
+	static void Travel_LMR(BST root) {
+		if(root == null) return;
+		
+		if(root.left != null) Travel_LMR(root.left);
+		System.out.print(root.key+" ");
+		if(root.right != null) Travel_LMR(root.right);
+	}
+	static void Travel_MLR(BST root) {
+		if(root == null) return;
+		System.out.print(root.key+"(" + root.priority + ") ");
+		if(root.left != null) Travel_MLR(root.left);
+		if(root.right != null) Travel_MLR(root.right);
+	}
+	
 	
 	static class BST {
 		static Random rand = new Random();
@@ -20,7 +46,7 @@ public class Main {
 		public BST(int key) {
 			super();
 			this.key = key;
-			this.priority = rand.nextInt();
+			this.priority = rand.nextInt(1000000000);
 			this.size = 1;
 			this.left = null;
 			this.right = null;
@@ -41,40 +67,106 @@ public class Main {
 			CalcSize();
 		}
 		
-		public void Rotate_Left() {
-			if(this.right == null) return;
-			BST newRoot = this.left;
+		public void Bottom_Up(BST upperNode) {
+			//피벗축이 루트인 경우
+			if(upperNode.parent == null) {
+				this.parent = null;
+				upperNode.parent = this;
+			}
+			//피벗이 루트가 아닌 경우,
+			else {
+				this.parent = upperNode.parent;
+				
+				//부모 쪽도 새로운 서브를 연결
+				if(upperNode.parent.left == upperNode) upperNode.parent.SetLeft(this);	
+				else if(upperNode.parent.right == upperNode) upperNode.parent.SetRight(this);
+				
+				upperNode.parent = this;
+			}
 			
-			this.SetLeft(newRoot.right);
-			newRoot.right.parent = this;
-			
-			newRoot.SetRight(this);
-			
-			//계승
-			if(this.parent.right == this) this.parent.right = newRoot;
-			else this.parent.left = newRoot;
-			
-			newRoot.parent = this.parent;
-			this.parent = newRoot;
 		}
-		public void Rotate_Right() {
 
-			if(this.left == null) return;
+		public BST Rotate_Left() {
+			if(this.right == null) return this;
 			BST newRoot = this.right;
 			
-			this.SetRight(newRoot.left);
-			newRoot.left.parent = this;
+			if(newRoot.left != null) {
+				this.SetRight(newRoot.left);
+				newRoot.left.parent = this;
+			}
+			else this.SetRight(null);
 			
 			newRoot.SetLeft(this);
 			
-			//계승
-			if(this.parent.right == this) this.parent.right = newRoot;
-			else this.parent.left = newRoot;
-			
-			newRoot.parent = this.parent;
-			this.parent = newRoot;
+			newRoot.Bottom_Up(this);
+			return newRoot;
 		}
+		public BST Rotate_Right() {
+			if(this.left == null) return this;
+			BST newRoot = this.left;
+			
+			if(newRoot.right != null) {
+				this.SetLeft(newRoot.right);
+				newRoot.right.parent = this;
+			}
+			else this.SetLeft(null);
+			
+			newRoot.SetRight(this);
+			
+			newRoot.Bottom_Up(this);
+			
+			return newRoot;
+		}
+	
 
+		// 1: 이진트리의 규칙을 지킨다.
+		// 2: 힙의 조건을 만족할때까지 로테이션 한다.
+		public void Insert(int number) {
+			BST node = new BST(number);
+			BST _parent = this.left;
+			
+			// [예외] 루트로 처음 삽입 시,
+			if(_parent == null) {
+				node.parent = this;
+				this.left = node;
+				return;
+			}
+			
+			
+			// [제1조건] node 삽입점 찾고 삽입.
+			while(true) {
+				if(_parent.key < number) {
+					if(_parent.right == null) {
+						_parent.SetRight(node);
+						node.parent = _parent;
+						break;
+					}
+					else _parent = _parent.right;
+				}
+				else {
+					if(_parent.left == null) {
+						_parent.SetLeft(node);
+						node.parent = _parent;
+						break;
+					}
+					else _parent = _parent.left;
+				}
+			}
+			// 삽입 종료
+			
+			// [제2조건] 완료까지 로테이트 반복
+			while(true) {
+				if(node.parent.priority < 0) break;
+				if(node.priority > _parent.priority) { 	
+					if(node == _parent.left) _parent = _parent.Rotate_Right();
+					if(node == _parent.right) _parent = _parent.Rotate_Left();
+					node = _parent;
+					_parent = _parent.parent;
+				}
+				else break;
+			}
+
+		}
 	}
 	
 	// ===================== functions for PS =====================
