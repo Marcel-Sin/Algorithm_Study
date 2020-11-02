e70b6f2f34124774228163ffa4e60efeba0198ae
MarcelSin <sys920525@gmail.com>
2020-11-02 11:36:08 +0900

    Eulerian Circuit and Trail

---
+++ Eulerian_Circuit/Main.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE; 
+	static final int INF = Integer.MAX_VALUE; 
+	static int TOTAL;
+	static int[][] MATRIX = new int[10][10];
+	static Stack<Integer> stack = new Stack<Integer>();
+	
+	public static void main(String[] args) throws IOException {
+		Connect(8,7);
+		Connect(7,5);
+		Connect(5,6);
+		Connect(6,0);
+		Connect(0,8);
+
+		
+		DFS(5);
+		
+		while(!stack.isEmpty()) System.out.print(stack.pop()+" ");
+		
+		
+		
+	}
+	
+	static void DFS(int here) {
+		for(int there = 0; there < MATRIX.length; there++) {
+			if(MATRIX[here][there] == 1) {
+				MATRIX[here][there] = 0;
+				DFS(there);
+			}
+		}
+		stack.add(here);
+	}
+	
+	
+	static void Connect(int a, int b) {
+		MATRIX[a][b] = 1;
+	}
+
+	
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		//System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("["+i+"] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
