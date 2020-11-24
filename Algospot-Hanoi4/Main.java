50189bdd0e928981ef01e42d24cdb5fa9de8c537
MarcelSin <sys920525@gmail.com>
2020-11-24 11:17:50 +0900

    Title : Complete Search using BFS, It was TLE

--- Algospot-Hanoi4/Main.java
+++ Algospot-Hanoi4/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -14,30 +14,119 @@ public class Main {
 	static IO_Manager io = new IO_Manager();
 	static final int NINF = Integer.MIN_VALUE; 
 	static final int INF = Integer.MAX_VALUE; 
-
 	
+	static int TOTAL;
+	
+	// checkMap <Hanoi4Data, Distance>
+	static HashMap<Integer, Integer> checkMap = new HashMap<Integer, Integer>();
+	static Queue<Integer> queue = new LinkedList<Integer>();
+	static int[] TWO_BIT_ACCESS = new int[16];
+	static int problemSize,problemData, finishData;
+	
+	//Temporary Memory for Method
+	static int[] mem = new int[20];
+	static int[] firstPlates = new int[4];
 	
 	public static void main(String[] args) throws IOException {	
-		System.out.println("Total Space Complexity : "+Calculate_Hanoi4_Space(12));
+		TOTAL = io.inputInt();
+		for (int i = 0; i < TOTAL; i++) {
+			Init();
+			System.out.println(BFS());
+		}
 	}
 	static void Init() throws IOException{
+		queue.clear();
+		checkMap.clear();
+		//Initializing for 2-bits indexing
+		int num = 0b11;
+		for (int i = TWO_BIT_ACCESS.length-1; i >= 0; i--) {
+			TWO_BIT_ACCESS[i] = num;
+			num = num << 2;
+		}
+		problemSize = io.inputInt();
+		
+		// Creating binary-datas for PS 
+		problemData = 0;
+		for(int pos = 0; pos < 4; pos++) {
+			StringTokenizer stk = new StringTokenizer(io.inputStr());
+			stk.nextToken();
+			while(stk.hasMoreTokens()) {
+				problemData = Set_2Bits(problemData,nextInt(stk)-1, pos);
+			}
+		}
+		finishData = 0;
+		for(int i = 0; i < problemSize; i++) {
+			finishData = Set_2Bits(finishData, i, 3);
+		}
 		
 	}
-	
-	static int Calculate_Hanoi4_Space(int n) {
-		// 4개의 서로 다른 상자에, 서로 다른 12개를 배치하는 경우의 수
-		return Pie(4, n);
-	}
-	
-
-	static int Pie(int n, int r) {
-		int ret = 1;
-		for(int i = 0; i < r; i++) {
-			ret *= n;
+	static int BFS() {
+		queue.add(problemData);
+		checkMap.put(problemData, 0);
+		
+		while(!queue.isEmpty()) {
+			int here = queue.poll();
+			
+			int[] adj = GetAdj(here);
+			for (int i = 0; i < adj.length; i++) {
+				if(adj[i] == -1) break;
+				if(checkMap.containsKey(adj[i]) == false) {
+					if(adj[i] == finishData) return checkMap.get(here)+1;
+					checkMap.put(adj[i],checkMap.get(here)+1);
+					queue.add(adj[i]);
+				}
+			}
 		}
-		return ret;
+		
+		return -1;
 	}
 	
+	static int[] GetAdj(int data){
+		Arrays.fill(mem, -1);
+		Arrays.fill(firstPlates, -1);
+		
+		
+		//각 기둥의 맨앞 원판을 찾는다.
+		int plate = 0, pos = 0, loop=0;
+		while(plate < problemSize && loop < firstPlates.length) {
+			pos = Get_2Bits(data, plate);
+			if(firstPlates[pos] == -1) {
+				firstPlates[pos] = plate;
+				loop++;
+			}
+			plate++;
+		}
+		
+		// 각 기둥의 맨앞 원판이 다른 기둥보다 크면 해당 자리로 옮겨본다.
+		int adjCount = 0;
+		for(int p1 = 0; p1 < firstPlates.length; p1++) {
+			if(firstPlates[p1] == -1) continue;
+			for(int p2 = 0; p2 < firstPlates.length; p2++) {
+				if(p1 == p2) continue;
+				if(firstPlates[p1] < firstPlates[p2] || firstPlates[p2] == -1) {
+					mem[adjCount++] = Set_2Bits(data, firstPlates[p1], p2);
+				}
+			}	
+		}
+		
+		return mem;
+	}
+	
+	// 2비트 단위로 데이터를 가져온다.
+	static int Get_2Bits(int data, int idx) {
+		return (data & TWO_BIT_ACCESS[idx]) >>> (30-2*idx);
+	}
+	// 2비트 단위로 데이터를 저장한다.
+	static int Set_2Bits(int data, int idx, int value) {
+		
+		// 해당 Data의 자리를 0으로 만든다.
+		data = data & (~TWO_BIT_ACCESS[idx]);
+		// 넣을 데이터의 자리를 맞춘다.
+		value = value << (30-2*idx);
+		
+		// 넣고 반환
+		return data | value;
+	}
 	
 	// ===================== functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
