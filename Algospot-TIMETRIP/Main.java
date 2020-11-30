f83eecc9d4860b99ad2d47a7568fdab8033a92bd
MarcelSin <sys920525@gmail.com>
2020-12-01 07:29:24 +0900

    Title : Bellman about min, max

---
+++ Algospot-TIMETRIP/Main.java
@@ -0,0 +1,179 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = -1000000000; 
+	static final int INF = 1000000000;
+	static final int PATH_CHECK = 100000000;
+	
+	static final int MAX_VERTEX = 100; 
+	
+	static int TOTAL,V,E;
+	
+	static ArrayList<Adjacent>[] GRAPH = new ArrayList[MAX_VERTEX];
+	static int[] minDist = new int[MAX_VERTEX];
+	static int[] maxDist = new int[MAX_VERTEX];
+	
+	
+	public static void main(String[] args) throws IOException{
+		for (int i = 0; i < GRAPH.length; i++) GRAPH[i] = new ArrayList<Main.Adjacent>(); 
+		TOTAL = io.inputInt();
+		for (int i = 0; i < TOTAL; i++) {
+			Init();
+			Solve();
+		}
+	}
+
+	
+	static void Init() throws IOException {
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		V = nextInt(stk);
+		E = nextInt(stk);
+		for (int i = 0; i < GRAPH.length; i++) GRAPH[i].clear();
+		
+		for (int i = 0; i < E; i++) {
+			stk = new StringTokenizer(io.inputStr());
+			Connect(nextInt(stk),nextInt(stk),nextInt(stk));
+		}
+		
+		
+	}
+	
+	static void Solve() {
+		boolean[] check = Bellman(0,GRAPH,V);
+		if(check[0] == true && minDist[1] == INF) {
+			System.out.println("UNREACHABLE");
+			return;
+		}
+		
+		if(check[0] == true && minDist[1] != INF ) System.out.printf("%d ",minDist[1]);
+		else System.out.print("INFINITY ");
+		
+		if(check[1] == true && maxDist[1] != INF ) System.out.printf("%d ",maxDist[1]);
+		else System.out.print("INFINITY ");
+		System.out.println();
+	}
+	
+	static boolean[] Bellman(int start, ArrayList<Adjacent>[] graph, int graphSize) {
+		boolean[] ret = new boolean[2];
+		
+		Arrays.fill(minDist, INF);
+		Arrays.fill(maxDist, NINF);
+		
+		minDist[start] = 0;
+		maxDist[start] = 0;
+		
+		boolean min_Updated = false , max_Updated = false;
+		int there, thereCost;
+		
+		for (int iter = 0; iter < graphSize; iter++) {
+			min_Updated = false;
+			max_Updated = false;
+			for (int v = 0; v < graphSize; v++) {
+				for (int i = 0; i < graph[v].size(); i++) {
+					there = graph[v].get(i).a;
+					thereCost = graph[v].get(i).b;
+					if(ret[0] == false && minDist[v] != INF && minDist[v] + thereCost < minDist[there]) {
+						minDist[there] = minDist[v] + thereCost;
+						min_Updated = true;
+					}
+					if(ret[1] == false && maxDist[v] != NINF && maxDist[v] + thereCost > maxDist[there]) {
+						maxDist[there] = maxDist[v] + thereCost;
+						max_Updated = true;
+					}
+				}
+			}
+			//graphSize - 1 회까지가 정상. 1회 더 업데이트 되면 Null			
+			if(min_Updated == false) ret[0] = true;
+			if(max_Updated == false) ret[1] = true;
+		}
+		if(min_Updated == true) ret[0] = false;
+		if(max_Updated == true) ret[1] = false;
+		
+		return ret;
+	}
+	
+	static void Connect(int a, int b, int w) {
+		GRAPH[a].add(new Adjacent(b,w));
+	}
+	static class Adjacent {
+		public int a,b;
+
+		public Adjacent(int a, int b) {
+			super();
+			this.a = a;
+			this.b = b;
+		}
+	}
+	
+	
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		//System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("["+i+"] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
