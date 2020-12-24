0d5053a3af6f5c9bd7149196713aee83539cb0f6
MarcelSin <sys920525@gmail.com>
2020-12-25 00:04:06 +0900

    My best slow PS code, avg 180ms, my Code : 3720ms

---
+++ BOJ-1699/Main.java
@@ -0,0 +1,109 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE / 4;
+	static final int MAX_SIZE = 100001;
+	
+	static int[] DP = new int[MAX_SIZE];
+	static int TOTAL,N,ANS;
+	
+	
+	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve(N));
+	}
+
+	static void Init() throws IOException{
+		N = io.inputInt();
+		DP[0] = 0;
+		Arrays.fill(DP, -1);
+		for (int i = 1; i*i <= 100000; i++) DP[i*i] = 1;	
+	}
+	//DP[N] : 자연수 N에 대한 제곱수의 최소 갯수
+	static int Solve(int n) {
+		if(DP[n] != -1) return DP[n];
+		int rootValue = (int)Math.sqrt(n);
+		int ret = Integer.MAX_VALUE;
+		for (int i = 1; i <= rootValue; i++) {
+			ret = Min(ret,DP[i*i] + Solve(n-i*i));
+		}
+		DP[n] = ret;
+		return DP[n];
+	}
+	
+
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
