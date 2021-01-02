0c29da47bb01a4e8a3baabba797b6609fc3eaa8a
MarcelSin <sys920525@gmail.com>
2021-01-03 04:27:26 +0900

    Review BOJ 11726,  136ms ( Rank TOP : 128ms )

---
+++ BOJ-11726/Main.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE / 4;
+	static final int MAX_SIZE = 1001;
+	static final int MOD = 10007;
+	
+	static int N;
+	static int[] DP = new int[MAX_SIZE];
+
+	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve(N));
+	}
+
+	static void Init() throws IOException {
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		N = nextInt(stk);
+		Arrays.fill(DP, -1);
+		DP[0] = INF;
+		DP[1] = 1;
+		DP[2] = 2;
+	}
+	static int Solve(int n) throws IOException {
+		if(DP[n] != -1) return DP[n];
+		DP[n] = Solve(n-1)+Solve(n-2);
+		return DP[n] %= MOD;
+	}
+
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
