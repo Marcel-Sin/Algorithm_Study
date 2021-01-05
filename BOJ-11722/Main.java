05e3e3c31e54c501ca5de81b230b98e2edfbd891
MarcelSin <sys920525@gmail.com>
2021-01-06 07:39:52 +0900

    Review BOJ 11722,  168ms ( Rank TOP : 136ms )

---
+++ BOJ-11722/Main.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX_SIZE = 1001;
+
+	static int N;
+	static int[] MAP = new int[MAX_SIZE];
+	static int[] DP = new int[MAX_SIZE];
+	
+	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve(N));
+	}
+
+	static void Init() throws IOException {
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		N = nextInt(stk);
+		stk = new StringTokenizer(io.inputStr());
+		for (int i = 1; i <= N; i++) MAP[i] = nextInt(stk);
+
+		DP[1] = 1;
+	}
+	static int Solve(int n) throws IOException {
+		for (int here = 2; here <= n; here++) {
+			int maxLen_MoreThan = 0;
+			for (int i = 1; i < here; i++) {
+				if(MAP[i] > MAP[here]) maxLen_MoreThan = Max(maxLen_MoreThan,DP[i]);
+			}
+			DP[here] = maxLen_MoreThan+1;
+		}
+		int ret = 0;
+		for (int i = 1; i <= n; i++) ret = Max(DP[i],ret); 
+		return ret;
+	}
+
+	// ===================== functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
