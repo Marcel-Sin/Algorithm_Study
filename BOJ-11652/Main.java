17beed21064d589ebb71fc4c45f20feadea25327
MarcelSin <sys920525@gmail.com>
2021-01-13 14:55:35 +0900

    Review BOJ 11652,  372ms ( Rank TOP : 328ms )

---
+++ BOJ-11652/Main.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE / 4;
+	static final int MAX_SIZE = 10001;
+	
+	
+	static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+	static int N,ANS_COUNT;
+	static long ANS;
+	
+	public static void main(String[] args) throws IOException {
+		Init();
+		Solve();
+	}
+
+	static void Init() throws IOException{
+		N = io.inputInt();
+		ANS_COUNT = 0;
+		ANS = Long.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			long key = io.inputLong();
+			int count = map.getOrDefault(key, 0);
+			count++;
+			map.put(key, count);
+			if(ANS_COUNT == count) {
+				ANS = Min(ANS,key);
+				ANS_COUNT = count;
+			}
+			else if(ANS_COUNT < count) {
+				ANS = key;
+				ANS_COUNT = count;
+			}
+		}
+	}
+
+	static void Solve() {
+		System.out.print(String.valueOf(ANS));
+	}
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
