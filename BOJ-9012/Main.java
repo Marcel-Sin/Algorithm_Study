edd8d62508bb4915872202156a5f41d9e9f22c92
MarcelSin <sys920525@gmail.com>
2021-01-15 15:05:39 +0900

    Review BOJ 9012, 144 ms ( Rank TOP : 132ms )

---
+++ BOJ-9012/Main.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.EmptyStackException;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE;
+	
+	static int TOTAL;
+	static char[] problem;
+	
+	public static void main(String[] args) throws IOException {
+		TOTAL = io.inputInt();
+		for (int i = 0; i < TOTAL; i++) {
+			Init();
+			boolean check = Solve(problem);
+			if(check) System.out.println("YES");
+			else System.out.println("NO");
+			
+		}
+
+	}
+
+	static void Init() throws IOException{
+		problem = io.inputStr().toCharArray();
+	}
+	
+	static boolean Solve(char[] problem) {
+		if(problem.length % 2 == 1) return false;
+		Stack<Character> stack = new Stack<Character>();
+		try {
+			for (int i = 0; i < problem.length; i++) {
+				char c = problem[i];
+				if(c == '(') stack.add(c);
+				else stack.pop();
+			}	
+		} catch(EmptyStackException e) {
+			return false;
+		}
+		if(stack.size() == 0) return true;
+		else return false;
+	}
+
+	
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+// ************************************** //
+// *-------------IO_Manager--------------* //
+// ************************************** //
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
