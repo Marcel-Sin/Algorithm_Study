018a5d912f8e49227ddd47ae3db762a7c5b59810
MarcelSin <sys920525@gmail.com>
2021-01-18 15:29:02 +0900

    Review BOJ 11655, 132ms ( Rank TOP : 128ms )

--- BOJ-11655/Main.java
+++ BOJ-11655/Main.java
@@ -3,54 +3,91 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE;
+
+	static char[] problem;
 	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		StringBuilder sb = new StringBuilder(io.inputStr());
-		for (int i = 0; i < sb.length(); i++) {
-			sb.setCharAt(i, (char)ROT13(sb.charAt(i)));
+	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(ROT13(problem));
+	}
+	
+	static void Init() throws IOException{
+		problem = io.inputStr().toCharArray();
+	}
+	
+	static String ROT13(char[] arr) throws IOException{
+		for (int i = 0; i < arr.length; i++) {
+			int here = arr[i];
+			if('a' <= here && here <= 'z') {
+				here = here - 'a';
+				here = (here + 13) % 26;
+				arr[i] = (char)(here+'a');
+			}
+			else if('A' <= here && here <= 'Z') {
+				here = here - 'A';
+				here = (here + 13) % 26;
+				arr[i] = (char)(here+'A');
+			}
+			else continue;
 		}
-		io.write(sb.toString());
-		io.close();
-		
-		
-		
-		
-		
-		
-	}		
-	static public int nextTokenInt(StringTokenizer stk) {
+		return new String(arr);
+	}
+
+	
+	
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	
-	static public int ROT13(int c) {
-		int temp = c;
-		if ('a' <= temp && temp <= 'z') {
-			temp += 13;
-			if ('z' < temp) temp = 'a' + (temp-'z'-1); 
-			return temp;
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
 		}
-		else if ('A' <= temp && temp <= 'Z') {
-			temp += 13;
-			if ('Z' < temp) temp = 'A' + (temp-'Z'-1); 
-			return temp;
-		}
-		else return c;
+		System.out.println();
 	}
 }
-
-
-
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -58,7 +95,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
