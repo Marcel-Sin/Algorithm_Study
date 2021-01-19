8a9f0189b0f7c2d59ac855dbb2df8e7274c3aa16
MarcelSin <sys920525@gmail.com>
2021-01-19 17:26:23 +0900

    Review BOJ 1406, Only 796ms ( Rank TOP : 440ms )

--- BOJ-1406/Main.java
+++ BOJ-1406/Main.java
@@ -3,85 +3,141 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ListIterator;
+import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE;
 
-		IO_Manager io = new IO_Manager();
-		
-		String str = io.inputStr();
-		int count = io.inputInt();
-		String[] orders = new String[count];
-		StringTokenizer stk;
-		Editor edit = new Editor(str);
-		
-		for(int i = 0; i < count; i++) orders[i] = io.br.readLine();
-		
-		for (int i = 0; i < count; i++) {
-			stk = new StringTokenizer(orders[i]);
-			Character opcode = stk.nextToken().charAt(0);
-			if (opcode == 'L') edit.Left();
-			else if (opcode == 'P') edit.Plus(stk.nextToken().charAt(0));
-			else if (opcode == 'B') edit.BackSpace();
-			else if (opcode == 'D') edit.Drive();
+	static int N;
+	static MyEditor editor = new MyEditor();
+	
+	public static void main(String[] args) throws IOException {
+		Init();
+		Solve();
+
+	}
+	
+	static void Init() throws IOException{
+		char[] str = io.inputStr().toCharArray();
+		N = io.inputInt();
+		for (int i = 0; i < str.length; i++) {
+			editor.Plus(str[i]);
 		}
-		io.write(edit.Result());
-		io.close();
-	}		
-	static public int nextTokenInt(StringTokenizer stk) {
+	}
+	static void Solve() throws IOException{
+		for (int i = 0; i < N; i++) {
+			StringTokenizer stk = new StringTokenizer(io.inputStr());
+			int command = stk.nextToken().charAt(0);
+			int operand = 0;
+			switch(command) {
+				case 'P':
+					operand = stk.nextToken().charAt(0);
+					editor.Plus((char)operand);
+					break;
+				case 'B':
+					editor.Back();
+					break;
+				case 'L':
+					editor.Left();
+					break;
+				case 'D':
+					editor.Drive();
+					break;
+			}
+		}
+		System.out.println(editor);
+	}
+	
+	//L,D,B,P x
+	static class MyEditor {
+		Stack<Character> backStack = new Stack<Character>();
+		Stack<Character> frontStack = new Stack<Character>();
+		
+		
+		public void Left() {
+			if(!backStack.empty()) {
+				frontStack.add(backStack.pop());
+			}
+		}
+		
+		public void Drive() {
+			if(!frontStack.empty()) {
+				backStack.add(frontStack.pop());
+			}
+		}
+		
+		public void Back() {
+			if(!backStack.empty()) {
+				backStack.pop();
+			}
+		}
+		
+		public void Plus(char c) {
+			backStack.add(c);
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			ListIterator<Character> iter = backStack.listIterator();
+			while(iter.hasNext()) sb.append(iter.next());
+			iter = frontStack.listIterator(frontStack.size());
+			while(iter.hasPrevious()) sb.append(iter.previous());
+			return sb.toString();
+		}
+	}
+
+
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
-}
-
-
-class Editor {
-	private LinkedList<Character> txt;
-	private ListIterator<Character> iter;
-	
-	public Editor(String str) {
-		super();
-		this.txt = new LinkedList<Character>();
-		for (int i = 0; i < str.length(); i++) txt.add(str.charAt(i));
-		iter = txt.listIterator(txt.size());
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
 	}
-	
-	public void Left() {
-		if (iter.hasPrevious() == false) return;
-		iter.previous();
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
 	}
-	public void Drive() {
-		if(iter.hasNext() == false) return;
-		iter.next();
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
 	}
-	public void BackSpace() {
-		if (iter.hasPrevious() == false) return;
-		iter.previous();
-		iter.remove();
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
 	}
-	public void Plus(char ch) {
-		iter.add(ch);
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
 	}
-	public String Result() {
-		char[] arr = new char[txt.size()];
-		iter = txt.listIterator(0);
-		for (int i = 0; i < txt.size(); i++) {
-			arr[i] = iter.next();
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
 		}
-		return String.valueOf(arr);
+		System.out.println();
 	}
 }
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -89,7 +145,7 @@ class Editor {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
