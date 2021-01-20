5b726ec048e377a4b1c711ab2eeec41f2e4bd492
MarcelSin <sys920525@gmail.com>
2021-01-20 16:45:27 +0900

    Review BOJ 1158, Only 304ms ( Rank TOP : 144ms )

--- BOJ-1158/Main.java
+++ BOJ-1158/Main.java
@@ -3,49 +3,104 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 4;
+	static final int INF = Integer.MAX_VALUE;
+
+	static int N,K;
+	static LinkedList<Integer> list = new LinkedList<Integer>();
+	static ArrayList<Integer> ans = new ArrayList<Integer>();
 	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		Vector<Integer> vint = new Vector<Integer>();
-		Vector<Integer> result = new Vector<Integer>();
-		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		int count = nextTokenInt(stk);
-		int moveSpeed = nextTokenInt(stk)-1;
-		int choice = 0;
-		for (int i=0; i < count; i++) vint.add(i+1);
-		
-		int size = count;
-		for (int i=0; i < count; i++) {
-			choice = (choice + moveSpeed) % size;
-			size--;
-			result.add(vint.elementAt(choice));
-			vint.remove(choice);
-		}
-		//<3, 6, 2, 7, 5, 1, 4>
-		
-		io.write("<");
-		for (int i = 0; i < count; i++) {
-			if (i == count-1) io.write(result.elementAt(i)+">");
-			else io.write(result.elementAt(i)+", ");
-		}
-		io.close();
-		
+	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve());
+
 	}
+	
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		N = nextInt(stk);
+		K = nextInt(stk);
+		for (int i = 1; i <= N; i++) {
+			list.add(i);
+		}
+	}
+	// 1 4 7
+	static String Solve() throws IOException{
+		ListIterator<Integer> iter = list.listIterator();
+		for (int i = 0; i < N; i++) {
+			int count = K;
+			int num = 0;
+			while(count != 0) {
+				if(iter.hasNext()) { num = iter.next(); count--;}
+				else iter = list.listIterator();
+			}
+			iter.remove();
+			ans.add(num);
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append('<');
+		for (int i = 0; i < ans.size()-1; i++) {
+			sb.append(ans.get(i));
+			sb.append(", ");
+		}
+		sb.append(ans.get(ans.size()-1));
+		sb.append('>');
 		
-	static public int nextTokenInt(StringTokenizer stk) {
+		return sb.toString();
+	}
+	
+
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -53,7 +108,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
