960169e4e74ee7d7d586f9a0f544a143d811f5e1
MarcelSin <sys920525@gmail.com>
2021-01-23 06:07:14 +0900

    Review BOJ 11005, 136ms ( Rank TOP : 132ms )

--- BOJ-11005/Main.java
+++ BOJ-11005/Main.java
@@ -3,49 +3,96 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.StringTokenizer;
-import java.util.Vector;
+import java.util.TreeMap;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		Vector<Integer> vint = new Vector<Integer>();
-		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		int number = nextTokenInt(stk);
-		int radix = nextTokenInt(stk);
-		int value = number,rest = 0;
-		
-		
-		while(value != 0) {
-			rest = value % radix;
-			vint.add(rest);
-			value = (int)(value/radix);
-		}
-		
-		for (int i=vint.size()-1; i >= 0; i--) {
-			int n = vint.elementAt(i);
-			if (n > 9) {
-				n = 'A'+(n-10);
-				char c = (char)n;
-				io.write(c+"");
-			}
-			else io.write(n+"");
-		}
-		io.close();
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
 
-		
-
-		
-		
+	static int N,B;
+	static StringBuilder sb = new StringBuilder();	
+	public static void main(String[] args) throws IOException {	
+		Init();
+		Solve();
+		System.out.println(sb.toString());
 	}
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		N = nextInt(stk);
+		B = nextInt(stk);
+	}
+	
+	static void Solve() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int value = N;
+		int r = 0;
+		while(value >= B) {
+			r = value % B;
+			list.add(r);
+			value = value / B;
+		}
+		if(value != 0) list.add(value);
 		
-	static public int nextTokenInt(StringTokenizer stk) {
+		for (int i = list.size()-1; i >= 0; i--) {
+			sb.append(Int_To_Char(list.get(i)));
+		}
+	}
+	
+	static char Int_To_Char(int n) {
+		if(n <= 9) return (char)(n+'0');
+		else {
+			n -= 10;
+			return (char)(n+'A');
+		}
+	}
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -53,7 +100,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
