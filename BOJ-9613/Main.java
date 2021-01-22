834d800236645fdda4f69367d13731c2b7d26e45
MarcelSin <sys920525@gmail.com>
2021-01-23 05:34:08 +0900

    Review BOJ 9613, 132ms ( Rank TOP : 124ms )

--- BOJ-9613/Main.java
+++ BOJ-9613/Main.java
@@ -3,60 +3,105 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.StringTokenizer;
-import java.util.Vector;
+import java.util.TreeMap;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		Vector<Long> results = new Vector<Long>();
-		int count = io.inputInt();
-		int[][] arr = new int[count][];
-		StringTokenizer stk;
-		
-		for (int i=0; i < count; i++) {
-			stk = new StringTokenizer(io.inputStr());
-			int loopCount = nextTokenInt(stk);
-			arr[i] = new int[loopCount];
-			for (int j = 0; j < loopCount; j++) arr[i][j] = nextTokenInt(stk);
-		}
-		// ---  input End. ---
-		
-		for(int i = 0; i < count; i++) {
-			long temp = 0;
-			for(int a = 0; a < arr[i].length-1; a++) {
-				for(int b = a+1; b < arr[i].length; b++) {
-					temp += GCD(arr[i][a],arr[i][b]);
-				}
-			}
-			results.add(temp);
-		}
-		
-		for(long n : results) io.write(n+"\n");
-		io.close();
-		
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
 
-		
-		
+	static int TOTAL;
+	static ArrayList<Integer> list = new ArrayList<Integer>();
+	
+	public static void main(String[] args) throws IOException {	
+		TOTAL = io.inputInt();
+		for (int i = 0; i < TOTAL; i++) {
+			Init();
+			System.out.println(Solve());
+		}
 	}
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		int n = nextInt(stk);
+		list.clear();
+		while(stk.hasMoreTokens()) list.add(nextInt(stk));
+	}
+	
+	static long Solve() {
+		long ret = 0;
+		int size = list.size();
 		
-	static public int nextTokenInt(StringTokenizer stk) {
+		for (int i = 0; i < size; i++) {
+			for (int j = i+1; j < size; j++) {
+				ret += GCD(list.get(i),list.get(j));
+			}
+		}
+		return ret;
+	}
+	
+	static int GCD(int a, int b) {
+		if(a > b) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		int r = -1;
+		while(r != 0) {
+			r = b % a;
+			if(r == 0) return a;
+			b = a;
+			a = r;
+		}
+		return -1;
+	}
+
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
-	static public int GCD(int a, int b) {
-		int rest, temp1=a, temp2=b;
-		while(true) {
-			rest = temp1 % temp2;
-			if (rest == 0) break;
-			temp1 = temp2;
-			temp2 = rest;
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
 		}
-		return temp2;
+		System.out.println();
 	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -64,7 +109,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
