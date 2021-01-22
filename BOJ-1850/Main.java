0a29e10836c1e4536b43ae174fafcd4855178308
MarcelSin <sys920525@gmail.com>
2021-01-22 17:40:26 +0900

    Review BOJ 1850, 288ms ( Rank TOP : 164ms )

--- BOJ-1850/Main.java
+++ BOJ-1850/Main.java
@@ -3,38 +3,99 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		long a = Long.parseLong(stk.nextToken());
-		long b = Long.parseLong(stk.nextToken());
-		if (a < b ) {long temp = a; a = b; b = temp;}
-		long rest, temp1 = a, temp2 = b;
-		while(true) {
-			rest = temp1 % temp2;
-			if (rest == 0) break;
-			temp1 = temp2;
-			temp2 = rest;
-		}
-		for(int i=0; i < temp2; i++) sb.append("1");
-		System.out.println(sb);
-		
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
 
-		
+	static long A,B;
+
+	
+	public static void main(String[] args) throws IOException {
+
+			Init();
+			Solve();
+
+
+	}
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		A = Long.parseLong(stk.nextToken());
+		B = Long.parseLong(stk.nextToken());
+		if(A > B) {
+			long temp = A;
+			A = B;
+			B = temp;
+		}
 		
 	}
+	
+	static void Solve() {
+		long rest = 0;
+		while(true) {
+			rest = B % A;
+			if(rest == 0) break;
+			B = A;
+			A = rest;
+		}
 		
-	static public int nextTokenInt(StringTokenizer stk) {
+		char[] ret = new char[(int)A];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = '1';
+		}
+		System.out.println(new String(ret));
+	}
+
+
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -42,7 +103,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
