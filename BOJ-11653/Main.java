8be8356d19c9f3601d6975b324d39c8900ac4c9b
MarcelSin <sys920525@gmail.com>
2021-01-27 08:52:28 +0900

    Review BOJ 11653, 168ms ( Rank TOP : 132ms )

--- BOJ-11653/Main.java
+++ BOJ-11653/Main.java
@@ -3,34 +3,90 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
+	static final int MAX = 10000001;
 	
-	public static void main(String[] args) throws IOException{
-		IO_Manager io = new IO_Manager();
-		int number = io.inputInt();
-		int value = number;
+	static int N;
+
+	
+	public static void main(String[] args) throws IOException {	
+		Init();
+		Solve();
+	}
+	static void Init() throws IOException{
+		N = io.inputInt();
+	}
+	
+	
+	static void Solve() throws IOException{
+		
+		int value = N;
+		int div = 2;
+		StringBuilder sb = new StringBuilder();
 		while(value != 1) {
-			for(int x = 2; x <= value; x++) {
-				if(value % x == 0) {
-					value = value/x;
-					io.write(x+"\n");
-					break;
-				}
+			if(value % div == 0) {
+				value /= div;
+				sb.append(div);
+				sb.append('\n');
 			}
+			else div++;
 		}
-		io.close();
+		System.out.println(sb.toString());
 	}
 
-	
 
-	static public int nextTokenInt(StringTokenizer stk) {
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
@@ -38,7 +94,7 @@ public class Main {
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
-	
+
 	public IO_Manager() {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
