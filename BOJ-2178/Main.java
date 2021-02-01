b340425c47d69ddc69612077c047ce4ec6be072b
MarcelSin <sys920525@gmail.com>
2021-02-02 08:36:53 +0900

    Review BOJ 2178, 152ms ( Rank TOP : 132ms )

--- BOJ-2178/Main.java
+++ BOJ-2178/Main.java
@@ -6,72 +6,126 @@ import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int MAX = 100;
+	
+	static int ROW,COL;
+	
+	static int[][] matrix = new int[MAX][MAX];
+	static boolean[][] visit = new boolean[MAX][MAX];
+	
+	static int[] dirRow = {-1,1,0,0};
+	static int[] dirCol = {0,0,-1,1};
+	
+	static Queue<Pair> queue = new LinkedList<Main.Pair>();
 
-	public static void main(String[] args) throws IOException {
-		IO_Manager io = new IO_Manager();
-		Vector<Node> passNode = new Vector<Node>();
-		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		int row=nextInt(stk),col = nextInt(stk);
-		int[][] maze = new int[row][col];
-		
-		for(int i=0; i<row; i++) {
-			char[] temp = io.inputStr().toCharArray();
-			for(int j=0; j<col; j++) maze[i][j] = temp[j]-48;
-		}
-		
-		BFS(maze,passNode,new Node(0,0,0));
-		System.out.println(passNode.elementAt(0).past);
-		
-		
-		
-	}
-	static int nextInt(StringTokenizer stk) {
-		return Integer.parseInt(stk.nextToken());
-	}
-
-	static void BFS(int[][] map, Vector<Node> passedQueue, Node start) {
-		Queue<Node> nodeQueue = new LinkedList<Node>();
-		map[start.row][start.col] = 2;
-		nodeQueue.add(start);
-		while(nodeQueue.isEmpty() == false) {
-			Node curPos = nodeQueue.poll();
-			curPos.past += 1;
-			if(curPos.row == map.length-1 && curPos.col == map[0].length-1) passedQueue.add(curPos);
-			FindingPath(map, nodeQueue, curPos);
-		}
+	public static void main(String[] args) throws IOException {	
+		Init();
+		System.out.println(Solve());
 	}
 	
-	static void FindingPath(int[][] map, Queue<Node> queue,Node cur) {
-		Node[] dir = new Node[4];
-		if(cur.row-1 >= 0 && map[cur.row-1][cur.col] == 1) dir[0] = new Node(cur.row-1,cur.col,cur.past);
-		if(cur.col-1 >= 0 && map[cur.row][cur.col-1] == 1) dir[1] = new Node(cur.row,cur.col-1,cur.past);
-		if(cur.row+1 < map.length && map[cur.row+1][cur.col] == 1) dir[2] = new Node(cur.row+1,cur.col,cur.past);
-		if(cur.col+1 < map[0].length && map[cur.row][cur.col+1] == 1) dir[3] = new Node(cur.row,cur.col+1,cur.past);
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		ROW = nextInt(stk);
+		COL = nextInt(stk);
 		
-		for(int i = 0; i<dir.length; i++) {
-			if(dir[i] != null) {
-				map[dir[i].row][dir[i].col] = 2;
-				queue.add(dir[i]);
+		for (int i = 0; i < ROW ; i++) {
+			char[] temp = io.inputStr().toCharArray();
+			int k = 0;
+			for (int j = 0; j < temp.length; j++) {
+				matrix[i][j] = temp[j]-'0';
 			}
 		}
 	}
 	
-}
-
-class Node {
-	public int row;
-	public int col;
-	public int past;
-	public Node(int row, int col, int past) {
-		super();
-		this.row = row;
-		this.col = col;
-		this.past = past;
+	static int Solve() throws IOException {
+		return BFS(0,0);
 	}
 	
+	// 함수 정의 : 해당 지점에서 목표까지의 최단거리 항상 리턴
+	static int BFS(int r, int c) {
+		queue.clear();
+		queue.add(new Pair(r,c));
+		visit[r][c] = true;
+		boolean change = false;
+		int ret = 1,nr,nc,size;
+		while(!queue.isEmpty()) {
+			change = false;
+			size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Pair pair = queue.poll();
+				for (int j = 0; j < 4; j++) {
+					nr = pair.a+dirRow[j]; nc = pair.b+dirCol[j];
+					if(0 <= nr&&nr < ROW && 0 <= nc&&nc < COL && matrix[nr][nc] == 1 && !visit[nr][nc]) {
+						if(nr == ROW-1 && nc == COL-1) return ret+1;
+						queue.add(new Pair(nr,nc));
+						visit[nr][nc] = true;
+						change = true;
+					}
+				}
+			}
+			if(!change) break;
+			else ret++;
+		}
+		return ret;
+	}
+	
+	static class Pair {
+		public int a,b;
+
+		public Pair(int a, int b) {
+			super();
+			this.a = a;
+			this.b = b;
+		}
+	}
+		
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			System.out.print("[" + i + "] : ");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
 
 // ************************************** //
