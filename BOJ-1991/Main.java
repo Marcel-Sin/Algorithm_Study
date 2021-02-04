6e862256271f96bc70d3713d8785d045378af7c4
MarcelSin <sys920525@gmail.com>
2021-02-04 15:09:01 +0900

    Review BOJ 1991, 132ms ( Rank TOP : 128ms )

--- BOJ-1991/Main.java
+++ BOJ-1991/Main.java
@@ -3,91 +3,104 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int MAX = 100;
+	
+	static int N;
+	
+	static char[] left = new char[100];
+	static char[] right = new char[100];
+	
 
-	public static void main(String[] args) throws IOException {
-		IO_Manager io = new IO_Manager();
-		HashMap<Character,Node> btree = new HashMap<Character,Node>();
-		StringTokenizer stk;
-		int count = io.inputInt();
-		for(int i=0; i<count; i++) {
-			stk = new StringTokenizer(io.inputStr());
-			char nodeContent = stk.nextToken().charAt(0);
-			char leftNode = stk.nextToken().charAt(0);
-			char rightNode = stk.nextToken().charAt(0);
-			btree.put(nodeContent, new Node(btree,nodeContent,leftNode,rightNode));
+	public static void main(String[] args) throws IOException {	
+		Init();
+		
+		MLR('A');
+		System.out.println();
+		LMR('A');
+		System.out.println();
+		LRM('A');
+	}
+	static void MLR(int here) {
+		System.out.print((char)here);
+		if(left[here] != '.') MLR(left[here]);
+		if(right[here] != '.') MLR(right[here]);
+	}
+	static void LMR(int here) {
+		if(left[here] != '.') LMR(left[here]);
+		System.out.print((char)here);
+		if(right[here] != '.') LMR(right[here]);
+	}
+	static void LRM(int here) {
+		if(left[here] != '.') LRM(left[here]);
+		if(right[here] != '.') LRM(right[here]);
+		System.out.print((char)here);
+	}
+	static void Init() throws IOException{
+		N = io.inputInt();
+		Arrays.fill(left, '.');
+		Arrays.fill(right, '.');
+		for (int i = 0; i < N; i++) {
+			StringTokenizer stk = new StringTokenizer(io.inputStr());
+			char c = stk.nextToken().charAt(0);
+			if(c < 65) continue;
+			left[c] = stk.nextToken().charAt(0);
+			right[c] = stk.nextToken().charAt(0);
 		}
+		
+	}
 
 		
-		Preorder(btree,btree.get('A'));
-		System.out.println();
-		inorder(btree,btree.get('A'));
-		System.out.println();
-		postorder(btree,btree.get('A'));
-		
-	
-	}
-	
-	static void Preorder(HashMap<Character,Node> btree, Node root) {
-		if (root == null) return;
-		System.out.print(root.content);
-		Preorder(btree,root.left());
-		Preorder(btree,root.right());
-	}
-	static void inorder(HashMap<Character,Node> btree, Node root) {
-		if (root == null) return;
-		inorder(btree,root.left());
-		System.out.print(root.content);
-		inorder(btree,root.right());
-	}
-	static void postorder(HashMap<Character,Node> btree, Node root) {
-		if (root == null) return;
-		postorder(btree,root.left());
-		postorder(btree,root.right());
-		System.out.print(root.content);
-	}
-	
-	
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-class Node {
-	public HashMap<Character,Node> graph;
-	public char content;
-	public char leftKey;
-	public char rightKey;
-	public Node(HashMap<Character,Node> graph,int content, int left, int right) {
-		super();
-		this.graph = graph;
-		this.content = (char)content;
-		this.leftKey = (char)left;
-		this.rightKey = (char)right;
-	}
-
-	public Node left() {
-		if (leftKey == '.') return null;
-		return graph.get(leftKey);
-	}
-	
-	public Node right() {
-		if (rightKey == '.') return null;
-		return graph.get(rightKey);
-	}
-	
-}
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
