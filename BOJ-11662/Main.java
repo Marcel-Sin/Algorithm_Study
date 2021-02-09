bd60c0ddd78ee6c7e4a1779cd107f3cb34ff7f17
MarcelSin <sys920525@gmail.com>
2021-02-09 10:52:18 +0900

    Review BOJ 11662, 152ms ( Rank TOP : 132ms )

--- BOJ-11662/Main.java
+++ BOJ-11662/Main.java
@@ -5,81 +5,125 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-// 이 풀이는 https://doublik.tistory.com/6 사이트가 참조 됐습니다.
-
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
 	
-	public static void main(String[] args) throws IOException {
-		IO_Manager io = new IO_Manager();
-		Cord mr_Minho,mr_Gangho;
+	static double[] problem = new double[8];
+	static Pair Minho,Kangho;
+	
+	public static void main(String[] args) throws IOException {	
+		Init();
+		System.out.printf("%.10f",Solve());
+	}
+	static void Init() throws IOException{
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		mr_Minho = new Cord(nextDouble(stk),nextDouble(stk),nextDouble(stk),nextDouble(stk));
-		mr_Gangho = new Cord(nextDouble(stk),nextDouble(stk),nextDouble(stk),nextDouble(stk));
+		for (int i = 0; i < problem.length; i++) {
+			problem[i] = Double.parseDouble(stk.nextToken());
+		}
+		Minho = new Pair(problem[0],problem[1],problem[2],problem[3]);
+		Kangho = new Pair(problem[4],problem[5],problem[6],problem[7]);
+		
+	}
 
-		double p,q,low,high,min,distP,distQ,minimum=3e9;
-		low = 0d;
-		high = 100d;
-		while (high-low >= 1e-10) {
-			p = (2*low+1*high)/3;
-			q = (1*low+2*high)/3;
-			PercentageCord(mr_Minho, p);
-			PercentageCord(mr_Gangho, p);
-			distP = Distance(mr_Minho,mr_Gangho);
+	static double Solve() throws IOException {
+		double low = 0, p, q , high = 1.0d;
+		double distP,distQ;
+		double distLow = Distance_Pair_WalkUnit(Minho,Kangho,low);
+		double distHigh = Distance_Pair_WalkUnit(Minho,Kangho,high);
+		double ret = Min(distLow,distHigh);
+		while(high - low > 1.0e-10) {
+			p = (low*2+high*1)/3;
+			q = (low*1+high*2)/3;
+			distP = Distance_Pair_WalkUnit(Minho,Kangho,p);
+			distQ = Distance_Pair_WalkUnit(Minho,Kangho,q);
+			ret = Min(distQ,ret);
+			ret = Min(distP,ret);
 			
-			PercentageCord(mr_Minho, q);
-			PercentageCord(mr_Gangho, q);		
-			distQ = Distance(mr_Minho,mr_Gangho);
-			
-			minimum = Math.min(minimum, Math.min(distP,distQ));
 			if(distP >= distQ) low = p;
 			else high = q;
 		}
-		System.out.printf("%.8f\n",minimum);
-	}
-
-	
-	static double nextDouble(StringTokenizer stk) {
-		return Double.parseDouble(stk.nextToken());
+		return ret;
 	}
 	
-	static double Distance(Cord a, Cord b) {
-		double x = (b.curX-a.curX);
-		double y = (b.curY-a.curY);
-		double dist = Math.sqrt((x*x)+(y*y));
-		return dist;
-	}
-	
-	static void PercentageCord(Cord t, double percent) {
-		t.curX = t.startX+t.distX*(percent/100);
-		t.curY = t.startY+t.distY*(percent/100);
+	static double Distance_Pair_WalkUnit(Pair a, Pair b, double walkUnit) {
+		//두 좌표 쌍에 목적지까지 walkUnit 적용
+		double ax = a.x+a.destX_Add*walkUnit;
+		double ay = a.y+a.destY_Add*walkUnit;
+		
+		double bx = b.x+b.destX_Add*walkUnit;
+		double by = b.y+b.destY_Add*walkUnit;
+		
+		
+		//실제 거리 계산
+		double ret = Math.pow((bx - ax),2);
+		ret += Math.pow((by - ay),2);
+		return Math.sqrt(ret);
 	}
 	
 
-	static class Cord {
-		double curX,curY;
-		double startX,startY;	
-		double endX,endY;
-		double distX,distY;
-		public Cord(double startX, double startY, double endX, double endY) {
+	static class Pair {
+		public double x,y;
+		public double destX_Add,destY_Add;
+		
+		public Pair(double x, double y, double destX, double destY) {
 			super();
-			this.curX = startX;
-			this.curY = startY;
-			this.startX = startX;
-			this.startY = startY;
-			this.endX = endX;
-			this.endY = endY;
-			distX = endX-curX;
-			distY = endY-curY;
-		}
-		
+			this.x = x;
+			this.y = y;
+			this.destX_Add = destX-x;  
+			this.destY_Add = destY-y;
+		}		
+	}
 	
-		
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
