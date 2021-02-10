c847a105c1c70fe180fc43012556dae9b5494885
MarcelSin <sys920525@gmail.com>
2021-02-10 11:09:07 +0900

    Review BOJ 11728, 1424ms ( Rank TOP : 1392ms )

--- BOJ-11728/Main.java
+++ BOJ-11728/Main.java
@@ -3,78 +3,108 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.StringTokenizer;
 
-
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE;
+	static final int MAX = 1000000;
 	
-	public static void main(String[] args) throws IOException {
-		IO_Manager io = new IO_Manager();
+	
+	static int N,M;
+	static int[] A = new int[MAX];
+	static int[] B = new int[MAX];
+	
+	
+	public static void main(String[] args) throws IOException {	
+		Init();
+		Solve();
+	}
+	
+	static void Init() throws IOException{
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		int sizeA = nextInt(stk);
-		int[] arrA = new int[sizeA];
-		int sizeB = nextInt(stk);
-		int[] arrB = new int[sizeB];
-		int[] arrC = new int[sizeA+sizeB];
-		
+		N = nextInt(stk);
+		M = nextInt(stk);
+		stk = new StringTokenizer(io.inputStr());
+		for (int i = 0; stk.hasMoreTokens(); i++) A[i] = nextInt(stk);
 		
 		stk = new StringTokenizer(io.inputStr());
-		for (int i = 0; i < sizeA; i++) arrA[i] = nextInt(stk);
-		stk = new StringTokenizer(io.inputStr());
-		for (int i = 0; i < sizeB; i++) arrB[i] = nextInt(stk);
-
-		MergeSort(arrC,arrA,arrB);
-		for (int n : arrC) io.write(n+" ");
-		io.close();
+		for (int i = 0; stk.hasMoreTokens(); i++) B[i] = nextInt(stk);
 	}
-
-	static void MergeSort(int[] output, int[] arrA, int[] arrB) {
-		int complete = 0;
-		int a=0,b=0;
-		while(complete < output.length) {
-			if (a < arrA.length && b < arrB.length) {
-				if (arrA[a] < arrB[b]) {
-					output[complete] = arrA[a];
-					a++;
-				} else if (arrA[a] > arrB[b]) {
-					output[complete] = arrB[b];
-					b++;
-				}
-				else {
-					output[complete] = arrA[a];
-					a++;
-				}
-				complete++;
-			}
-			else {
-				if(a == arrA.length) {
-					for(int i = b; i < arrB.length; i++) {
-						output[complete] = arrB[i];
-						complete++;
-					}
-				}
-				else if(b == arrB.length) {
-					for(int i = a; i < arrA.length; i++) {
-						output[complete] = arrA[i];
-						complete++;
-					}
-				}
-				else break;
-			}
+	static void Solve() throws IOException{
+		StringBuilder sb = new StringBuilder();
+		int size = N+M,a = 0, b = 0,k=0;
+		int[] ret = new int[size]; 
+		while(k < size) {
+			if(N <= a) ret[k] = B[b++];
+			else if(M <= b) ret[k] = A[a++];
+			else if(A[a] < B[b]) ret[k] = A[a++];
+			else ret[k] = B[b++];
+			k++;
 		}
-		
+		for (int l = 0; l < size; l++) {
+			sb.append(ret[l]);
+			sb.append(' ');
+		}
+		System.out.println(sb.toString());
 	}
 	
+	
+	
+	
+
+	
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	
-	
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
