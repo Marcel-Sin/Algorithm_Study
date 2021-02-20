66d479e9c29bd27cecda287acadbd98e6453b692
MarcelSin <sys920525@gmail.com>
2021-02-20 14:06:30 +0900

    Review BOJ 1931, 592ms ( Rank TOP : 480ms )

--- BOJ-1931/Main.java
+++ BOJ-1931/Main.java
@@ -3,65 +3,113 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-
 public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int MAX = 100001;
+
+	static int N;
+	static Pair[] meeting = new Pair[MAX];
 	
 	public static void main(String[] args) throws IOException {
-		IO_Manager io = new IO_Manager();
-		int size = io.inputInt();
-		Discuss[] times = new Discuss[size];
+		Init();
+		System.out.println(Solve());
+	}
+	
+	static void Init() throws IOException{
+		N = io.inputInt();
 		
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < N; i++) {
 			StringTokenizer stk = new StringTokenizer(io.inputStr());
-			times[i] = new Discuss(nextInt(stk),nextInt(stk));
+			meeting[i] = new Pair(nextInt(stk),nextInt(stk));
 		}
-		Arrays.sort(times);
-		int finishTime = 0;
-		int count = 0;
-		for(int i = 0; i < size; i++) {
-			if(times[i].start >= finishTime) {
-				finishTime = times[i].end;
-				count++;
+		Arrays.sort(meeting,0,N);
+	}
+	static int Solve() throws IOException{
+		int ret = 0,current_Time=0;
+		//현재 시각에서 끝나는 시간이 가장 짧은 걸 택한다.
+		for (int i = 0; i < N; i++) {
+			if(current_Time > meeting[i].a) continue;
+			else {current_Time = meeting[i].b; ret++;}
+		}
+		return ret;
+	}
+	
+	static class Pair implements Comparable<Pair>{
+		public int a,b;
+
+		public Pair(int a, int b) {
+			super();
+			this.a = a;
+			this.b = b;
+		}
+
+		@Override
+		public int compareTo(Pair o) {
+			
+			if(this.b == o.b) {
+				if(this.a == o.a) return 0;
+				else return (this.a < o.a) ? -1:1;
 			}
+			else return (this.b < o.b) ? -1:1;
 		}
-		System.out.println(count);
-		
 		
 	}
 	
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
-
-	static class Discuss implements Comparable<Discuss>{
-		int start;
-		int end;
-		public Discuss(int start, int end) {
-			super();
-			this.start = start;
-			this.end = end;
-		}
-		
-		@Override
-		public int compareTo(Discuss o) {
-			if(this.end == o.end) {
-				if(this.start == o.start) return 0;
-				else return (this.start < o.start)? -1:1;
-			}
-			else return (this.end < o.end)? -1:1;
-		}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
 	}
-	
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
