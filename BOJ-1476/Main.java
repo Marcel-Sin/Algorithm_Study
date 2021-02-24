0996a188148c39b5b03f1898e710498413ce8170
MarcelSin <sys920525@gmail.com>
2021-02-24 10:34:45 +0900

    Review BOJ 1476 132ms ( Rank TOP : 124ms )

--- BOJ-1476/Main.java
+++ BOJ-1476/Main.java
@@ -3,45 +3,92 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE/2;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int MAX = 10001;
+	
+	static int E,S,M;
+	
 	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve());
+	}
+	
+	static void Init() throws IOException{
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		int E = nextInt(stk),S = nextInt(stk),M = nextInt(stk);
-		if(E == 15) E = 0;
-		if(S == 28) S = 0;
-		if(M == 19) M = 0;
+		E = nextInt(stk);
+		S = nextInt(stk);
+		M = nextInt(stk);
 		
-		int n = 1;
+	}
+
+	static int Solve() throws IOException {
+		int a = 1,b = 1 ,c = 1,ret = 1; 
 		while(true) {
-			if(n%15 == E && n%28 == S && n%19 == M) {
-				break;
-			}
-			n++;
+			if(a == E && b == S && c == M) break;
+			ret++;
+			a = (a+1 < 16) ? a+1:1;
+			b = (b+1 < 29) ? b+1:1;
+			c = (c+1 < 20) ? c+1:1;
 		}
-		System.out.println(n);
-	}	
-
-
+		return ret;
+	}
 	
 	
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-
-
-	
-	
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
