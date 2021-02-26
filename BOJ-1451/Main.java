235ad307de024f27f417899f820369f080c4fb30
MarcelSin <sys920525@gmail.com>
2021-02-26 16:48:22 +0900

    Review BOJ 1451 140ms ( Rank TOP : 132ms )

--- BOJ-1451/Main.java
+++ BOJ-1451/Main.java
@@ -3,146 +3,166 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int[][] arr;
-	static long[][] sum;
-	static int H,W;
-	static long ans = -1;
+	static final int NINF = Integer.MIN_VALUE/2;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int MAX = 101;
+	
+	static int N,M;
+	static int[][] map;
 	public static void main(String[] args) throws IOException {
+		Init();
+		System.out.println(Solve());
+	}
+	
+	static void Init() throws IOException{
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		H = nextInt(stk);
-		W = nextInt(stk);
-		arr = new int[H+1][W+1];
-		sum = new long[H+1][W+1];
-		for(int i = 1; i <= H; i++) {
-			char[] temp = io.inputStr().toCharArray();
-			for(int j = 0; j < W; j++) arr[i][j+1] = temp[j]-48;
+		N = nextInt(stk);
+		M = nextInt(stk);
+		map = new int[N+1][M+1];
+		for (int i = 1; i <= N; i++) {
+			char[] temp =  io.inputStr().toCharArray();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = temp[j-1]-'0';
+			}
 		}
-		/*
-      for (int i=1; i<=W; i++) {
-         for (int j=1; j<=W; j++) {
-             sum[i][j] = sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+(long)a[i][j];
-         }
-     }
-     */
-		CreateSum();
-		Case01();
-		Case02();
-		Case03();
-		Case04();
-		Case05();
-		Case06();
-		System.out.println(ans);
-	}	
-	static void CreateSum() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				map[i][j] += map[i][j-1];
+				map[i][j] += map[i-1][j];
+				map[i][j] -= map[i-1][j-1];
+			}
+		}
 		
-		int rowAdder = 0;
-			for(int i = 1; i <= W; i++) {
-				rowAdder += arr[1][i];
-				sum[1][i] = rowAdder;
-			}
-			if(H == 1) return;
-			
-			for(int y = 2; y <= H; y++) {
-				rowAdder = 0;
-				for(int x = 1; x <= W; x++) {
-					rowAdder += arr[y][x];
-					sum[y][x] = sum[y-1][x] + rowAdder;
-				}
-			}
 	}
 
-	static long RectSum(int sRow,int sCol,int eRow, int eCol) {
-		return sum[eRow][eCol] - sum[eRow][sCol-1] - sum[sRow-1][eCol] + sum[sRow-1][sCol-1];
-	}
+	static long Solve() throws IOException {
+		// 제한 범위 (시작은 inclusive , 종료는 exclusive)
+		long ret = 0,temp = 0;
 
-	static void Case01() {
-		for(int i = 1; i <= H - 2; i++) {
-			for(int j = i+1; j <= H - 1; j++) {
-				long r1 = RectSum(1,1,i,W);
-				long r2 = RectSum(i+1,1,j,W);
-				long r3 = RectSum(j+1,1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
+		// [Pattern-1] - Row
+		for (int a = 1; a <= N - 2; a++) {
+			for (int b = a + 1; b <= N - 1; b++) {
+				temp = Square(1, 1, a, M);
+				temp *= Square(a+1, 1 , b, M);
+				temp *= Square(b+1, 1, N, M);
+				ret = Max(ret, temp);
 			}
 		}
-	}
-	
-	static void Case02() {
-		for(int i = 1; i <= W-2; i++) {
-			for(int j = i+1; j <= W-1; j++) {
-				long r1 = RectSum(1,1,H,i);
-				long r2 = RectSum(1,i+1,H,j);
-				long r3 = RectSum(1,j+1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
+		
+		// [Pattern-2] - Col
+		for (int a = 1; a <= M - 2; a++) {
+			for (int b = a + 1; b <= M - 1; b++) {
+				temp = Square(1, 1, N, a);
+				temp *= Square(1, a + 1, N, b);
+				temp *= Square(1, b + 1, N, M);
+				ret = Max(ret, temp);
 			}
 		}
-	}
-	
-	static void Case03() {
-		for(int row = 1; row <= H-1; row++) {
-			for(int col = 1; col <= W-1; col++) {
-				long r1 = RectSum(1,1,row,W);
-				long r2 = RectSum(row+1,1,H,col);
-				long r3 = RectSum(row+1,col+1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
-			}
-		}
-	}
 
-	static void Case04() {
-		for(int row = 1; row <= H-1; row++) {
-			for(int col = 1; col <= W-1; col++) {
-				long r1 = RectSum(1,1,row,col);
-				long r2 = RectSum(row+1,1,H,col);
-				long r3 = RectSum(1,col+1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
+		// [Pattern-3] - small small large
+		for (int row = 1; row <= N - 1; row++) {
+			for (int col = 1; col <= M - 1; col++) {
+				temp = Square(1, 1, row, col);
+				temp *= Square(1, col+1 , row, M);
+				temp *= Square(row+1, 1, N, M);
+				ret = Max(ret, temp);
 			}
 		}
-	}
-	
-	static void Case05() {
-		for(int row = 1; row <= H-1; row++) {
-			for(int col = 1; col <= W-1; col++) {
-				long r1 = RectSum(1,1,row,col);
-				long r2 = RectSum(1,col+1,row,W);
-				long r3 = RectSum(row+1,1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
+
+		// [Pattern-4] - large small small
+		for (int row = 1; row <= N - 1; row++) {
+			for (int col = 1; col <= M - 1; col++) {
+				temp = Square(1, 1, row, M);
+				temp *= Square(row+1, 1 , N, col);
+				temp *= Square(row+1, col+1, N, M);
+				ret = Max(ret, temp);
 			}
 		}
-	}
-	
-	static void Case06() {
-		for(int row = 1; row <= H-1; row++) {
-			for(int col = 1; col <= W-1; col++) {
-				long r1 = RectSum(1,1,H,col);
-				long r2 = RectSum(1,col+1,row,W);
-				long r3 = RectSum(row+1,col+1,H,W);
-				long temp = r1*r2*r3;
-				if(ans < temp) ans = temp;
+		
+		// [Pattern-5] - large small small 
+		for (int col = 1; col <= M - 1; col++) {
+			for (int row = 1; row <= N - 1; row++) {
+				temp = Square(1, 1, N, col);
+				temp *= Square(1, col+1, row, M);
+				temp *= Square(row+1, col+1, N, M);
+				ret = Max(ret, temp);
 			}
 		}
+		
+		// [Pattern-6] - small small large
+		for (int col = 1; col <= M - 1; col++) {
+			for (int row = 1; row <= N - 1; row++) {
+				temp = Square(1, 1, row, col);
+				temp *= Square(row+1, 1, N, col);
+				temp *= Square(1, col+1, N, M);
+				ret = Max(ret, temp);
+			}
+		}
+		return ret;
+	}
+	private static int square_sum = 0;
+	static int Square(int r, int c, int r2, int c2) {
+		square_sum = map[r2][c2];
+		square_sum -= map[r-1][c2];
+		square_sum -= map[r2][c-1];
+		square_sum += map[r-1][c-1];		
+		return square_sum;
 	}
 	
-	
-	
+
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
