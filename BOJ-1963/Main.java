1f5cd86c081c672c3dad399f2943993141c83c37
MarcelSin <sys920525@gmail.com>
2021-03-03 11:51:37 +0900

    Review BOJ 1963 164ms ( Rank TOP : 144ms )

--- BOJ-1963/Main.java
+++ BOJ-1963/Main.java
@@ -8,113 +8,125 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int A,B,ANSWER;
-	static boolean[] notPrime = new boolean[10001];
-	static int[] check = new int[10001];
-	public static void main(String[] args) throws IOException {
-		int caseCount = io.inputInt();
-		Eratos();
-		for(int i = 0; i < caseCount; i++) {
-			Arrays.fill(check, -1);
-			StringTokenizer stk = new StringTokenizer(io.inputStr());
-			A = nextInt(stk);
-			B = nextInt(stk);
-			BFS(A);
-			if(check[B] == -1) System.out.println("Impossible");
-			else System.out.println(check[B]);
+	static final int NINF = Integer.MIN_VALUE/2;
+	static final int INF = Integer.MAX_VALUE/2;
+	static final int PROBLEM_MAX = 9999;
+	static final int PROBLEM_MIN = 1000;
+	static final int[] POW = {1,10,100,1000,10000};
+	
+	
+	static int N,M,TEST;
+	static int[] visited = new int[PROBLEM_MAX+1];
+	static boolean[] PRIME = new boolean[PROBLEM_MAX+1];
+	static Queue<Integer> queue = new LinkedList<Integer>();
+	
+	public static void main(String[] args) throws IOException{
+		Before_Init();
+		TEST = io.inputInt();
+		for (int i = 0; i < TEST; i++) {
+			Init();
+			Solve();
 		}
-		
-	}	
+	}
+	
+	static void Before_Init() throws IOException{
+		Arrays.fill(PRIME, true);
+		for (int i = 2; i < 100; i++) {
+			for (int j = 0; j < PRIME.length; j+=i) {
+				PRIME[j] = false;
+			}
+		}
+	}
 
-	static void BFS(int startNumber) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		check[startNumber] = 0;
-		queue.add(startNumber);
-		int counter = 0;
-		while(queue.isEmpty() == false) {
-			int loopCount = queue.size();
-			for(int loop = 0 ; loop < loopCount; loop++) {
-				int parent = queue.poll();
-				for(int i = 1; i <= 4; i++) {
-					for(int j = 0; j < 10; j++) {
-						int nextNum = ReplacePos(parent, i, j);
-						if(check[nextNum] == -1 &&  isPrime(nextNum) && nextNum >= 1000) {
-							check[nextNum] = counter+1;
-							queue.add(nextNum);
-						}
+	
+	static void Init() throws IOException{
+		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		N = nextInt(stk);
+		M = nextInt(stk);
+		Arrays.fill(visited, -1);
+	}
+
+	static void Solve() throws IOException {
+		queue.clear();
+		queue.add(N);
+		visited[N] = 0;
+		int next = 0;
+		while(!queue.isEmpty()) {
+			int here = queue.poll();
+			if(here == M) break;
+			for (int pos = 0; pos < 4; pos++) {
+				for (int num = 0; num < 10; num++) {
+					next = Numset(here,pos,num);
+					if(PROBLEM_MIN <= next && PRIME[next] && visited[next] == -1) {
+						queue.add(next);
+						visited[next] = visited[here]+1;
 					}
 				}
 			}
-			counter++;
 		}
+		if(visited[M] == -1) System.out.println(-1);
+		else System.out.println(visited[M]);
 	}
 	
-	static void Eratos() {
-		notPrime[1] = true;
-		for(int i = 2; i <= 100; i++) {
-			for(int j = i+i; j <= 10000; j+=i) notPrime[j] = true;
-		}
+	private static int unit,front,rear;
+	static int Numset(int value, int pos, int num) {
+		unit = POW[pos];
+		front = value/POW[pos+1];
+		rear = value % unit;
+		return num*unit + front*POW[pos+1] + rear;
 	}
 	
-	static int ReplacePos(int num,int pos,int replacer) {
-		int value = 0, rest = 0;
-		if(pos == 1) {
-			value = num % 1000;
-			replacer *= 1000;
-			return value+replacer;
-		}
-		else if(pos == 2) {
-			value = num / 1000;
-			value *= 1000;
-			rest = num % 100;
-			replacer *= 100;
-			return value+rest+replacer;
-		}
-		else if(pos == 3) {
-			value = num / 100;
-			value *= 100;
-			rest = num % 10;
-			replacer *= 10;
-			return value+rest+replacer;
-		}
-		else if(pos == 4) {
-			value = num / 10;
-			value *= 10;
-			return value+replacer;
-		}
-		return -1;
-	}
 	
-	static boolean isPrime(int n) {
-	  return (notPrime[n] == true)? false:true;
-	}
-	
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ============================================================
+	// ===================== functions for PS =====================
+	// ============================================================
+	// ============================================================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	static void Swap(int[] a, int i , int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] =temp;
-	}	
-	static void PartialReverse(int[] a, int start, int end) {
-		int temp;
-		while(end > start) {
-			temp = a[start];
-			a[start++] = a[end];
-			a[end--] = temp;
-		}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
 	}
-	
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		// System.out.println("요소갯수 : " + arr.length);
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ",arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
-
-
-
-
-
 
 // ************************************** //
 // *-------------IO_Manager--------------* //
