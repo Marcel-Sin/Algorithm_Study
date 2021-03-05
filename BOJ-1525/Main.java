6e614bae8e4c7858b5503d0cf1318cf062488cf5
MarcelSin <sys920525@gmail.com>
2021-03-05 12:51:50 +0900

    Review BOJ 1525 756ms ( Rank TOP : 392ms )

--- BOJ-1525/Main.java
+++ BOJ-1525/Main.java
@@ -3,179 +3,120 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static final int NINF = Integer.MIN_VALUE; 
-	static final int INF = Integer.MAX_VALUE; 
-
-	static int TOTAL;
-	static HashMap<Long, Integer> checkMap = new HashMap<Long, Integer>(); 
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int PROBLEM_MAX = 10000;
+	
+	static int N, M;
+	
+	static StringBuilder sb; 
 	static Queue<Long> queue = new LinkedList<Long>();
-	static int[][] problem = new int[3][3];
-	static int[][] finish = {{1,2,3},{4,5,6},{7,8,0}};
-	static long problemCode,finishCode;
-	static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+	static HashMap<Long, Integer> visited = new HashMap<Long, Integer>();
+	static long answer;
+	static long problem;
+	static int[] dirR = {0,0,-1,1};
+	static int[] dirC = {-1,1,0,0};
 	
 	
-	
-	
-	public static void main(String[] args) throws IOException {	
+	public static void main(String[] args) throws IOException {
 		Init();
-		System.out.println(BFS());
+		Solve();
+	
 	}
-	static void Init() throws IOException{
-		StringTokenizer stk;
-		for(int i = 0; i < 3; i++) {
-			stk = new StringTokenizer(io.inputStr());
+
+	static void Init() throws IOException {
+		int[][] arr = new int[3][3];
+		
+		for (int i = 0; i < 3; i++) {
+			StringTokenizer stk = new StringTokenizer(io.inputStr());
 			for (int j = 0; j < 3; j++) {
-				problem[i][j] = nextInt(stk);
+				int num = nextInt(stk);
+				if(num == 0) problem = SetPane(problem, i*3+j, 9);
+				else problem = SetPane(problem, i*3+j,num);
+				
 			}
 		}
-		
-		problemCode = ArrayToHash(problem);
-		finishCode = ArrayToHash(finish);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				answer = SetPane(answer,i*3+j,i*3+j+1);
+			}
+		}
 	}
-	static int Bi_BFS() {
-		if(problemCode == finishCode) return 0;
-		
-		queue.add(problemCode);
-		queue.add(finishCode);
-		
-		checkMap.put(problemCode, 1);
-		checkMap.put(finishCode, -1);
-		
-		
-		long hereCode,adjCode;
-		int hereCost,adjCost=0;
+
+	
+	static void Solve() throws IOException {
+		queue.clear();
+		queue.add(problem);
+		visited.put(problem, 0);
 		while(!queue.isEmpty()) {
-			hereCode = queue.poll();
-			hereCost = checkMap.get(hereCode);
-			
-			ArrayList<Long> adj = GetAdjacent(hereCode);
-			for(int i = 0; i < adj.size(); i++) {
-				adjCode = adj.get(i);
-				adjCost = checkMap.getOrDefault(adjCode, 0);
-				if(adjCost == 0) {
-					if(hereCost > 0) checkMap.put(adjCode, hereCost+1);
-					else checkMap.put(adjCode, hereCost-1);
-					queue.add(adjCode);
-				}
-				else if(sgn(hereCost) != sgn(adjCost)) {
-					return abs(hereCost) + abs(adjCost) - 1;
-				}
-			}
-		}
-		return -1;
-	}
-	static int BFS() {
-		if(problemCode == finishCode) return 0;
-		
-		queue.add(finishCode);
-		checkMap.put(finishCode, 0);
-		
-		long hereCode,adjCode;
-		int hereCost;
-		while(!queue.isEmpty()) {
-			hereCode = queue.poll();
-			hereCost = checkMap.get(hereCode);
-			
-			ArrayList<Long> adj = GetAdjacent(hereCode);
-			for(int i = 0; i < adj.size(); i++) {
-				adjCode = adj.get(i);
-				if(checkMap.containsKey(adjCode) == false) {
-					if(adjCode == problemCode) return hereCost+1;
-					queue.add(adjCode);
-					checkMap.put(adjCode, hereCost+1);
+			long here = queue.poll();
+			if(here == answer) break;
+			int herePos = GetPos(here,9);
+			int hereCount = visited.get(here);
+			int row = herePos/3, col = herePos%3;
+			int nextPos,temp;
+			for (int i = 0; i < dirR.length; i++) {
+				if(0 <= row+dirR[i] && row+dirR[i] < 3 && 0 <= col+dirC[i] && col+dirC[i] < 3) {
+					nextPos = (row+dirR[i])*3 + col+dirC[i];
+					//다음 위치에 존재하는 값을 임시보관
+					temp = GetPosValue(here, nextPos);
+					//다음 위치를 9로 바꿈
+					long there = SetPane(here, nextPos, 9);
+					//현재 위치는 다음위치에 있던 값이 옴
+					there = SetPane(there, herePos, temp);
+					if(visited.containsKey(there) == true) continue;
+					visited.put(there, hereCount+1);
+					queue.add(there);
 				}
 			}
 		}
-		return -1;
-	}
-
-	static int abs(int n) {
-		if(n >= 0) return n;
-		else return n*(-1);
-	}
-	static int sgn(int n) {
-		if(n >= 0) return 1;
-		else return -1;
+		
+		if(visited.containsKey(answer) == false) System.out.println(-1);
+		else System.out.println(visited.get(answer));
 	}
 	
-	static long ArrayToHash(int[][] arr) {
-		long ret = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				ret = ret | arr[i][j];
-				ret = ret << 4;
-			}
-		}
-		ret = ret >>> 4;
-		return ret;
-	}
-	
-	static ArrayList<Long> GetAdjacent(long hereCode) {
-		ArrayList<Long> ret = new ArrayList<Long>();
-		int curR, curC;
-		int zeroPos = GetPos(hereCode, 0);
-		curR = zeroPos/3; curC = zeroPos%3;
+	private static long tempA;
+	static long SetPane(long curValue, int pos, int setValue) {
+		// Step.1 : 원하는 곳을 0000로 만든다.
+		pos = 4*(8-pos);
+		tempA = 0b1111L << pos;
+		tempA = ~tempA;
+		curValue = tempA & curValue;
+		//System.out.println(Long.toBinaryString(curValue));
 		
-		for (int i = 0; i < 4; i++) {
-			int nr = curR+dir[i][0];
-			int nc = curC+dir[i][1];
-			if(nr >= 0 && nc >= 0 && nr < 3 && nc < 3) {
-				long nextCode = SwapPos(hereCode,curR,curC,nr,nc);
-				ret.add(nextCode);
-			}
-		}
-		return ret;
-	}
-	static long SwapPos(long code, int r, int c, int swapR, int swapC) {
-		int pos = r*3+c, pos2 = swapR*3+swapC;
-		int temp = GetNum(code, pos);
-		code = SetNum(code, GetNum(code, pos2), pos);
-		code = SetNum(code, temp, pos2);
+		// Step.2 : 원하는 곳 위치한 숫자를 만든다.
+		tempA = setValue;
+		tempA = tempA << pos;
 		
-		return code;
+		// Step.3 : 비어있는 녀석과 넣을 녀석을 OR 후 리턴한다.
+		//System.out.println(Long.toBinaryString(curValue | tempA));
+		return curValue | tempA;
 	}
-
-	static long SetNum(long code, int num, int pos) {
-		int shifter = (8-pos)*4;
-		//Bits Clear
-		long temp = (long)0b1111 << shifter;
-		temp = ~temp;
-		code = temp & code;
-		
-		//Set Bits
-		temp = (long)num << shifter;
-		code = temp | code;
-		return code;
-	}
-	static int GetNum(long code,int pos) {
-		int shift = (8-pos)*4;
-		long temp = (long)0b1111 << shift;
-		temp = temp & code;
-		temp = temp >> shift;
-		return (int)temp;
-	}
-	static int GetPos(long code,int num) {
-		for (int i = 8; i >=  0; i--) {
-			if( (code & 0b1111) == num) return i;
-			code = code >> 4;
+	private static long tempB;
+	static int GetPos(long curValue, long findValue) {
+		for (int p = 0; p < 9; p++) {
+			tempB = (curValue >> 4*(8-p)) & 0b1111l;
+			if(tempB == findValue) return p;
 		}
 		return -1;
 	}
-
+	static int GetPosValue(long curValue, int pos) {
+		tempA = 0b1111L << 4*(8-pos);
+		tempA = tempA & curValue;
+		tempA = tempA >> 4*(8-pos);
+		return (int)tempA;
+	}
 	
-	
-	
-	// ===================== functions for PS =====================
+//	===================== ETC functions for PS =====================
+//	===================== ETC functions for PS =====================
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
@@ -191,8 +132,14 @@ public class Main {
 	static int Max(int a, int b) {
 		return (a > b) ? a : b;
 	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
 	static void Display(int[] arr, int limit) {
-		//System.out.println("요소갯수 : " + arr.length);
+		// System.out.println("요소갯수 : " + arr.length);
 		for (int i = 0; i < limit; i++)
 			System.out.print(arr[i] + " ");
 		System.out.println();
@@ -200,9 +147,8 @@ public class Main {
 	static void Display(int[][] arr, int limit) {
 		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
 		for (int i = 0; i < limit; i++) {
-			System.out.print("["+i+"] : ");
-			for (int j = 0; j < arr[0].length; j++) {
-				System.out.print(arr[i][j] + " ");
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ", arr[i][j]);
 			}
 			System.out.println();
 		}
