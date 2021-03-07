5c4fec55df2392084180d89f39f36d699e282752
MarcelSin <sys920525@gmail.com>
2021-03-07 12:14:12 +0900

    Review BOJ 2186 484ms ( Rank TOP : 396ms )

--- BOJ-2186/Main.java
+++ BOJ-2186/Main.java
@@ -3,99 +3,135 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+import java.util.TreeSet;
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int N,M,K;
-	static char[][] MAP;
-	static char[] TARGET;
-	static int TARGET_LEN;
-	static int[][][] DP;
-	static int SUM = 0;
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int PROBLEM_MAX = 101;
+	
+	static int N,M,K,ans,searchWord_LastIDX;
+	
+	static char[][] charMap;
+	static char[] searchWord;
+	
+	static int[][][] checkMap;
+	static int[][] dirR;
+	static int[][] dirC;
+	
+	
+	
 	public static void main(String[] args) throws IOException {
-		Initializing();
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(MAP[i][j] == TARGET[0]) SUM += DFS(i,j,0);
-			}
-		}
-		System.out.println(SUM);
-	}	
-
-	//DFS 정의 : [i,j]에서 시작한 문자열의 경로의 수 탐색. 시작은 DFS(i,j,0);
-	static int DFS(int i, int j, int target_idx) {
-		// Basic Condition
-		if (DP[i][j][target_idx] != -1) return DP[i][j][target_idx];
-		if ( target_idx == TARGET_LEN) return 1;
-		
-		// Retrieve ABCD
-		DP[i][j][target_idx] = 0;
-		for(int n = 1; n <= K; n++) {
-			if(i-n >= 0 && MAP[i-n][j] == TARGET[target_idx+1]) DP[i][j][target_idx] += DFS(i-n,j,target_idx+1);
-			if(i+n < N && MAP[i+n][j] == TARGET[target_idx+1]) DP[i][j][target_idx] += DFS(i+n,j,target_idx+1);
-			if(j-n >= 0 && MAP[i][j-n] == TARGET[target_idx+1]) DP[i][j][target_idx] += DFS(i,j-n,target_idx+1);
-			if(j+n < M && MAP[i][j+n] == TARGET[target_idx+1]) DP[i][j][target_idx] += DFS(i,j+n,target_idx+1);
-		}
-		return DP[i][j][target_idx];
+		Init();
+		System.out.println(Solve());
 	}
-	
-	static void Initializing() throws IOException{
+
+	static void Init() throws IOException {
+		//1 <= K <= 5 이므로
+		int[] dr = {0,0,-1,1};
+		int[] dc = {-1,1,0,0};
+		dirR = new int[6][4]; dirC = new int[6][4];
+		for (int k = 1; k <= 5; k++) {
+			for (int dir = 0; dir < 4; dir++) {dirR[k][dir] = dr[dir]*k; dirC[k][dir] = dc[dir]*k;	}
+		}
+		
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		N = nextInt(stk);
-		M = nextInt(stk);
-		K = nextInt(stk);
-		MAP = new char[N][M];
-	
-		for(int i = 0; i<N; i++) {
-			char[] temp = io.inputStr().toCharArray();
-			for(int j = 0; j<M; j++) MAP[i][j] = temp[j];
-		}	
-		TARGET = (io.inputStr()+" ").toCharArray();
-		TARGET_LEN = TARGET.length-2;
+		N = nextInt(stk);		M = nextInt(stk);		K = nextInt(stk);
 		
-		DP = new int[100][100][80];
-		for(int i = 0; i < 100; i++) {
-			for(int j = 0; j < 100; j++) {
-				Arrays.fill(DP[i][j], -1);
+		charMap = new char[N][];
+		for (int i = 0; i < N; i++) charMap[i] = io.inputStr().toCharArray();
+		searchWord = io.inputStr().toCharArray();
+		searchWord_LastIDX = searchWord.length-1;
+		
+		checkMap = new int[N][M][searchWord.length];
+		for (int i = 0; i < N; i++) 
+			for (int j = 0; j < M; j++)
+				for (int z = 0; z < searchWord.length; z++)	checkMap[i][j][z] = -1;
+	}
+
+	static int Solve() throws IOException {
+		ans = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(charMap[i][j] == searchWord[0]) {
+					ans += DFS(i,j,0);
+					
+				}
 			}
 		}
+		return ans;
+	}
+	static int DFS(int curR, int curC, int curCount) {
+		if(curCount == searchWord_LastIDX) {return 1;}
+		int nr,nc,nextCount;
+		nextCount = curCount+1;
+		checkMap[curR][curC][curCount] = 0;
+		for (int k = 1; k <= K; k++) {
+			for (int i = 0; i < 4; i++) {
+				nr = curR+dirR[k][i]; 
+				nc = curC+dirC[k][i];
+				if(0 <= nr && nr < N && 0 <= nc && nc < M && searchWord[nextCount] == charMap[nr][nc]) {
+					if(checkMap[nr][nc][nextCount] == -1) checkMap[curR][curC][curCount] += DFS(nr,nc,nextCount);
+					else checkMap[curR][curC][curCount] +=checkMap[nr][nc][nextCount];
+				}
+			}
+		}
+		return checkMap[curR][curC][curCount];
 	}
 	
 	
-
 	
+	
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	static void Swap(int[] a, int i , int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] =temp;
-	}	
-	static void PartialReverse(int[] a, int start, int end) {
-		int temp;
-		while(end > start) {
-			temp = a[start];
-			a[start++] = a[end];
-			a[end--] = temp;
-		}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
 	}
-	
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		System.out.println("요소갯수 : " + (arr.length * arr[0].length));
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf("%2d ", arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
 
 
-
-
-
-
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
