d9a8bf8a0718003071e32af62e300af10910c428
MarcelSin <sys920525@gmail.com>
2021-03-12 10:43:15 +0900

    Review BOJ 1987 956ms, Too Slowgit add *! ( Rank TOP : 176ms )

--- BOJ-1987/Main.java
+++ BOJ-1987/Main.java
@@ -4,93 +4,98 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static char[][] MAP;
-	static int R,C,ANS = 0;
-	static State state = new State();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+
+	static int R,C,ans;
+	static char[][] map;
+	static boolean[] checked = new boolean[100]; 
+	static int[] dirR = {0,0,-1,1};
+	static int[] dirC = {-1,1,0,0};
+	
 	public static void main(String[] args) throws IOException {
-		Initializing();
-		state.Select(0, 0);
-		DFS(state,0,0);
-		System.out.println(ANS);
+		Init();
+		Solve();
 	}
 	
-	static void DFS(State s,int row, int col) {
-		if(ANS == 26) return;
-		
-		int north = row-1, south = row+1, east = col+1, west = col-1;
-		if(south < R && s.Check(MAP[south][col]) == false) {
-			s.Select(south, col);
-			DFS(s,south,col);
-			s.Deselect(south, col);
-		}
-		if(east < C && s.Check(MAP[row][east]) == false) {
-			s.Select(row, east);
-			DFS(s,row,east);
-			s.Deselect(row, east);
-		}
-		if(north >= 0 && s.Check(MAP[north][col]) == false) {
-			s.Select(north, col);
-			DFS(s,north,col);
-			s.Deselect(north, col);
-		}
-		if(west >= 0 && s.Check(MAP[row][west]) == false) {
-			s.Select(row, west);
-			DFS(s,row,west);
-			s.Deselect(row, west);	
-		}
-		
-	}
-	
-	static class State {
-		int count;
-		boolean[] check;
-		
-		public State() {
-			check = new boolean[26];
-			count = 0;
-		}
-		
-		public void Select(int r, int c) {
-			char ch = MAP[r][c];
-			check[ch-65] = true;
-			count++;
-			if(ANS < count) ANS = count;
-		}
-		
-		public void Deselect(int r, int c) {
-			char ch = MAP[r][c];
-			check[ch-65] = false;
-			count--;
-		}
-		
-		public boolean Check(char c) {
-			return check[c-65];
-		}
-	}
-	
-	static void Initializing() throws IOException{
+	static void Init() throws IOException {
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
-		R = nextInt(stk);
-		C = nextInt(stk);
-		MAP = new char[R][];
-		for (int i = 0; i < R; i++) MAP[i] = io.inputStr().toCharArray();
+		R = nextInt(stk); C = nextInt(stk);
+		map = new char[R][];	
+		for (int i = 0; i < R; i++) map[i] = io.inputStr().toCharArray();
+		ans = 0;
 	}
 	
+	static void Solve() throws IOException {
+		checked[map[0][0]] = true;
+		DFS(0,0,1);
+		System.out.println(ans);
+	}
 	
+	static void DFS(int r, int c,int counter) throws IOException {
+		checked[map[r][c]] = true;
+		
+		ans = Max(ans,counter);
+		if(26 <= ans) return;
+		
+		int nr, nc;
+		for (int i = 0; i < 4; i++) {
+			nr = r+dirR[i]; nc = c+dirC[i];
+			if(0 <= nr && nr < R && 0 <= nc && nc < C && is_Duplicated(nr, nc) == false) {
+				DFS(nr,nc,counter+1);
+				checked[map[nr][nc]] = false;
+			}
+		}
+	}
+	
+	static boolean is_Duplicated(int i, int j) {
+		return checked[map[i][j]];
+	}
+
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
 
 
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
