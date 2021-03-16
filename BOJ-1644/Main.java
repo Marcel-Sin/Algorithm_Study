8450ae87dd421b3e5efe796d6e5a45810652d017
MarcelSin <sys920525@gmail.com>
2021-03-17 07:43:09 +0900

    Review BOJ 1644 300ms ( Rank TOP : 184ms )

--- BOJ-1644/Main.java
+++ BOJ-1644/Main.java
@@ -4,59 +4,93 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static int TARGET = 0,COUNT = 0;
-	static boolean[] PRIME;
-	static int[] PRIME_LIST = new int[283147];
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+
+	
+	static int N,SIZE;
+	static ArrayList<Integer> list = new ArrayList<Integer>(500000);
+	
 	public static void main(String[] args) throws IOException {
-		long start = System.currentTimeMillis();
-		Initializing();
-		
-		int left = 0, right = 0, sum = 0, size = PRIME_LIST.length,before = 0;
+		Init();
+		System.out.println(Solve());
+	}
+	
+	static void Init() throws IOException {
+		N = io.inputInt();
+		boolean[] notPrime = new boolean[N+1];
+		int root = Math.abs(N)+1;
+		notPrime[1] = true;
+		for (int i = 2; i <= root; i++) {
+			for (int j = i+i; j <= N; j+=i) {
+				notPrime[j] = true;
+			}
+		}
+		for (int i = 2; i <= N; i++) {
+			if(!notPrime[i]) list.add(i);
+		}
+		SIZE = list.size();
+	}
+	static int Solve() throws IOException {
+		int ret = 0;
+		int i=0,j=0,sum=0;
 		while(true) {
-			if(sum >= TARGET) sum -= PRIME_LIST[left++];
-			else if(right == size) break;
-			else {
-				before = PRIME_LIST[right++];
-				sum += before;
+			if(sum < N && j < SIZE) sum += list.get(j++);
+			else if(sum >= N && i < j) {
+				sum -= list.get(i++);
 			}
-			
-			if(sum == TARGET) COUNT++;
-			if(before > TARGET) break;
+			if(sum == N) ret++;
+			if(j == SIZE && sum < N) break;
 		}
-		System.out.println(COUNT);
+		return ret;
 	}
 	
-	static void Initializing() throws IOException{
-		PRIME = new boolean[4000001];
-		//Arrays.fill(PRIME,true);
-		PRIME[0] = true;
-		PRIME[1] = true;
-		int eratos_Size = 2000;
-		for(int i = 2; i <= eratos_Size; i++) {
-			for(int j = i+i; j <= 4000000; j+=i) {
-				PRIME[j] = true;
-			}
-		}
-		int c = 0;
-		for(int i = 2; i <= 4000000; i++) if(PRIME[i] == false) PRIME_LIST[c++] = i;
-		TARGET = io.inputInt();
-	}
 	
+
+
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
 
 
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
