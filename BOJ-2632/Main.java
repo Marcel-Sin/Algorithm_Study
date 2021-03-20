59af2dcbd5e368f544d1af2ff92dfeb78ae09d4d
MarcelSin <sys920525@gmail.com>
2021-03-21 07:26:33 +0900

    Review BOJ 2632 228ms ( Rank TOP : 184ms )

--- BOJ-2632/Main.java
+++ BOJ-2632/Main.java
@@ -4,110 +4,118 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
-	static final int INF = Integer.MAX_VALUE;
-	static int NEED;
-	static int[] A,B;
-	static int N,M;
-	static long c1,c2,COUNTER;
-	static ArrayList<Integer> v1 = new ArrayList<Integer>();
-	static ArrayList<Integer> v2 = new ArrayList<Integer>();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000001;
+
+	
+	static int NEED,N,M;
+	static int[] A = new int[MAX];
+	static int[] B = new int[MAX];
+	static ArrayList<Integer> pizzaListA = new ArrayList<Integer>();
+	static ArrayList<Integer> pizzaListB = new ArrayList<Integer>();
+	
+	
 	public static void main(String[] args) throws IOException {
 		Init();
-		Case_Create(A, v1);
-		Case_Create(B, v2);
-		v1.add(Integer.MAX_VALUE);
-		v2.add(Integer.MAX_VALUE);
-		Collections.sort(v1);
-		Collections.sort(v2);
-		
-		COUNTER = 0;
-		COUNTER += CountBetween(v1, NEED);
-		COUNTER += CountBetween(v2, NEED);
-		
-		int value = v1.get(1),rest = 0,temp;
-		while(true) {
-			rest = NEED-value;
-			if(Lower(v2,rest) != INF) {
-				c1 = CountBetween(v1, value);
-				c2 = CountBetween(v2, rest);
-				COUNTER += c1*c2;
-			}
-			temp = Upper(v1,value);
-			if(temp == INF) break;
-			else value = v1.get(temp);
-		}
-		System.out.println(COUNTER);
-
-	}
-	
-	static int Lower(ArrayList<Integer> v, int target) {
-		int s = 1, e = v.size()-1,m,mid;
-		while(s < e) {
-			m = (s+e)/2;
-			mid = v.get(m);
-			if(mid < target) s = m+1;
-			else e = m;
-		}
-		if(v.get(e) != target) return INF;
-		else return e;
-	}
-	static int Upper(ArrayList<Integer> v, int target) {
-		int s = 1, e = v.size()-1,m,mid;
-		while(s < e) {
-			m = (s+e)/2;
-			mid = v.get(m);
-			if(mid <= target) s = m+1;
-			else e = m;
-		}
-		if(v.get(e-1) != target) return INF;
-		else return e;
-	}
-	static int CountBetween(ArrayList<Integer> v, int target) {
-		return Upper(v,target) - Lower(v,target);
+		System.out.println(Solve());
 	}
 	
 	
-	
-	static void Case_Create(int[] a,ArrayList<Integer> v) {
-		int size = a.length,sum;
-		for(int s = 0; s < size; s++) {
-			sum = 0;
-			for(int i = 0; i < size-1; i++) {
-				sum += a[(s+i)%size];
-				v.add(sum);
-			}
-		}
-		sum = 0;
-		for(int i = 0; i < size; i++) sum += a[i];
-		v.add(sum);
-	}
-	static void Init() throws IOException{
+	static void Init() throws IOException {
 		NEED = io.inputInt();
-		StringTokenizer stk = new StringTokenizer(io.inputStr());
+		StringTokenizer stk =new StringTokenizer(io.inputStr());
 		N = nextInt(stk);
 		M = nextInt(stk);
-		A = new int[N];
-		for(int i = 0; i < N; i++) A[i] = io.inputInt();
-		B = new int[M];
-		for(int i = 0; i < M; i++) B[i] = io.inputInt();
-		v1.add(Integer.MIN_VALUE);
-		v2.add(Integer.MIN_VALUE);
+		for (int i = 0; i < N; i++) pizzaListA.add(io.inputInt());
+		for (int i = 0; i < M; i++) pizzaListB.add(io.inputInt());
 	}
+	static long Solve() throws IOException {
+		long ret = 0;
+		
+		//A,B 각각에 대해, 1조각씩, 2조각씩 , 3조각씩 ... n-1조각씩
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum = 0;
+			for (int piece = 0; piece < N-1; piece++) {
+				sum += pizzaListA.get((i+piece)%N);
+				CountUp(A,sum);
+			}
+			if(i == 0) CountUp(A,sum+pizzaListA.get(N-1));			//All 조각 Sum
+		}
+		for (int i = 0; i < M; i++) {
+			sum = 0;
+			for (int piece = 0; piece < M-1; piece++) {
+				sum += pizzaListB.get((i+piece)%M);
+				CountUp(B,sum);
+			}
+			if(i == 0) CountUp(B,sum+pizzaListB.get(M-1));
+		}
+		
+		ret += A[NEED];
+		ret += B[NEED];
+		//각 피자는 자연수이므로, [NEED-1]까지만 확인 한다.
+		for (int a = 1; a <= NEED-1; a++) {
+			ret += A[a]*B[NEED-a];
+		}
+		return ret;
+	}
+	
+	
+	
+	static void CountUp(int[] arr,int value) {
+		arr[value]++;
+	}
+	
+
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
 
 
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
