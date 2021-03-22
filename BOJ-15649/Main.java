ee3ac91f98db7b5308a1e6e2857c6f6676814ef9
MarcelSin <sys920525@gmail.com>
2021-03-23 07:54:46 +0900

    Review BOJ 15649 Next Permu Test ( Rank TOP : 200ms )

--- BOJ-15649/Main.java
+++ BOJ-15649/Main.java
@@ -3,93 +3,106 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
 	static int N,M;
+	static int[] problem;
+	
 	public static void main(String[] args) throws IOException {
-		Initializing();
-		State s = new State();
-		DFS(s);
-		io.close();
+		Init();
+		Solve();
 	}
 	
-	static void Initializing() throws IOException{
+	
+	static void Init() throws IOException {
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
 		N = nextInt(stk);
 		M = nextInt(stk);
-	}
-	
-	static void DFS(State state) throws IOException{
-		if(state.storage_Pointer == M) {
-			for(int i = 0; i < M; i++) { io.bw.write(state.storage[i]);
-			io.bw.write(' ');
-			}
-			io.bw.write('\n');
-			return;
-		}
 		
-		for(int i = 1; i <= N; i++) {
-			if(state.check[i] == false) {
-				state.Select(i);
-				DFS(state);
-				state.check[i] = false;
-				state.storage_Pointer--;
-			}
+		problem = new int[N];
+		for (int i = 0; i < problem.length; i++) problem[i] = i+1;
+	}
+	static void Solve() throws IOException {
+		//Reverse(problem,0,N);
+		while(Next_Permutation(problem) == true) {
+			Display(problem, M);
 		}
 	}
 	
-	static class State {
-		boolean[] check;
-		char[] storage;
-		int storage_Pointer;
-		public State() {
-			super();
-			check = new boolean[N+1];
-			storage = new char[M];
-			this.storage_Pointer = 0;
-		}
+	static boolean Next_Permutation(int[] arr) {
+		int size = arr.length;
+		if(size == 1) return false;
+		int i=-1,ii=size-1,j=size-1;
+		while(0 < ii && arr[ii-1] > arr[ii]) ii--; 	
+		if(ii == 0) return false;
 		
-		public void Select(int n) {
-			check[n] = true;
-			storage[storage_Pointer] = (char)(n+48);
-			storage_Pointer++;
+		i = ii-1;
+			while(arr[i] > arr[j]) j--;
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			Reverse(arr,ii,size);
+			return true;
+		
+	}
+	private static int left,right,temp;
+	static void Reverse(int [] arr,int inStart,int exEnd) {
+		left = inStart;
+		right = exEnd-1;
+		while(left < right) {
+			temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++; right--;
 		}
 	}
-		
-		
-		
-	
-	
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	static void Swap(int[] a, int i , int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] =temp;
-	}	
-	static void PartialReverse(int[] a, int start, int end) {
-		int temp;
-		while(end > start) {
-			temp = a[start];
-			a[start++] = a[end];
-			a[end--] = temp;
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
 		}
 	}
-	
 }
 
 
-
-
-
-
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
