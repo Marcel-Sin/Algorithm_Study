98573c50f124ffa6f8d34f226b98ee545495d449
MarcelSin <sys920525@gmail.com>
2021-03-25 11:43:34 +0900

    Review BOJ 15650 132ms ( Rank TOP : 124ms )

--- BOJ-15650/Main.java
+++ BOJ-15650/Main.java
@@ -3,90 +3,104 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
 	static int N,M;
+	static char[] problem;
+	static boolean[] used;
+	static char[] out;
+	
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
-		Initializing();
-		State s = new State();
-		DFS(s,0);
-		io.close();
+		Init();
+		Solve();
 	}
 	
-	static void Initializing() throws IOException{
+	
+	static void Init() throws IOException {
 		StringTokenizer stk = new StringTokenizer(io.inputStr());
 		N = nextInt(stk);
 		M = nextInt(stk);
+		
+		problem = new char[N];
+		used = new boolean[N];
+		out = new char[M*2];
+		for (int i = 1; i < out.length; i+=2) out[i] = ' ';	
+		out[M*2-1] = '\n';
+		
+		for (int i = 0; i < problem.length; i++) problem[i] = (char)(i+1+'0');
 	}
-	
-	static void DFS(State state,int curValue) throws IOException{
-		if(state.storage_Pointer == M) {
-			for(int i = 0; i < M; i++) { io.bw.write(state.storage[i]);
-			io.bw.write(' ');
-			}
-			io.bw.write('\n');
+	static void Solve() throws IOException {
+		sb = new StringBuilder();
+		DFS(-1,0);
+		System.out.print(sb.toString());
+	}
+
+	static void DFS(int here,int count) throws IOException {
+		if(count == M) {
+			sb.append(new String(out));
 			return;
 		}
 		
-		for(int i = curValue+1; i <= N; i++) {
-				state.Select(i);
-				DFS(state,i);
-				state.storage_Pointer--;
+		for (int i = here+1; i < N; i++) {
+			if(used[i] == false) {
+				used[i] = true;
+				out[count*2] = problem[i];
+				DFS(i,count+1);
+				used[i] = false;
+			}
 		}
 	}
-	
-	static class State {
-		boolean[] check;
-		char[] storage;
-		int storage_Pointer;
-		public State() {
-			super();
-			check = new boolean[N+1];
-			storage = new char[M];
-			this.storage_Pointer = 0;
-		}
-		
-		public void Select(int n) {
-			//check[n] = true;
-			storage[storage_Pointer] = (char)(n+48);
-			storage_Pointer++;
-		}
-	}
-		
-		
-		
-	
-	
+
+
+//	===================== ETC functions for PS =====================
 	static int nextInt(StringTokenizer stk) {
 		return Integer.parseInt(stk.nextToken());
 	}
-	static void Swap(int[] a, int i , int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] =temp;
-	}	
-	static void PartialReverse(int[] a, int start, int end) {
-		int temp;
-		while(end > start) {
-			temp = a[start];
-			a[start++] = a[end];
-			a[end--] = temp;
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
 		}
 	}
-	
 }
 
 
-
-
-
-
-// ************************************** //
-// *-------------IO_Manager--------------* //
-// ************************************** //
+//-------------IO_Manager--------------
 class IO_Manager {
 	public BufferedReader br;
 	public BufferedWriter bw;
