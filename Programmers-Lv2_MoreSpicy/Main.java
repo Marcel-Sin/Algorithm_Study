94e3334ac21dff6adee95e93f21da0e8c186e5c9
MarcelSin <sys920525@gmail.com>
2021-04-09 10:25:38 +0900

    Solve Lv.2 More spicy

---
+++ Programmers-Lv2_MoreSpicy/Main.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+
+
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
+	public Solution solve = new Solution();
+	static String[] genres;
+	static int[] plays;
+
+	public static void main(String[] args) throws IOException {
+		Execute();
+	}
+	
+	
+	static void Execute() {
+		Main mainObj = new Main();
+		int ans = mainObj.solve.solution(new int[] {1, 2, 3, 9, 10, 12},7);
+		System.out.println(ans);
+	}
+	
+	//import java.util.*;
+	class Solution {
+		public int solution(int[] scoville, int K) {
+			PriorityQueue<Long> pq = new PriorityQueue<Long>();
+			for (int i = 0; i < scoville.length; i++) pq.add((long)scoville[i]);
+			int ret = 0;
+			long a,b;
+			while(pq.peek() < K && 1 < pq.size()) {
+				pq.add(pq.poll()+pq.poll()*2);
+				ret++;
+			}
+			if(pq.peek() < K) return -1;
+			else return ret;
+		}
+	}
+
+
+//	===================== ETC functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+//-------------IO_Manager--------------
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
