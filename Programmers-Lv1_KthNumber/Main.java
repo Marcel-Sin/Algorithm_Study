2fdb234e1c9a129c1a568a4c54edd34428c84184
MarcelSin <sys920525@gmail.com>
2021-04-11 12:43:40 +0900

    Solve Lv.1 Kth-Number in Sorting

---
+++ Programmers-Lv1_KthNumber/Main.java
@@ -0,0 +1,116 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+
+
+
+
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
+	public Solution solve = new Solution();
+
+	public static void main(String[] args) throws IOException {
+		Execute();
+	}
+	
+	
+	static void Execute() {
+		Main mainObj = new Main();
+		int[] ans = mainObj.solve.solution(new int[] {1,5,2,6,3,7,4},new int[][] {{2,5,3},{4,4,1},{1,7,3}});
+		Display(ans, ans.length);
+	}
+	
+	//import java.util.*;
+	class Solution {
+		public int[] solution(int[] array, int[][] commands) {
+			
+			int[] ret = new int[commands.length];
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			for (int i = 0; i < array.length; i++) list.add(array[i]);	
+			for (int i = 0; i < commands.length; i++) {
+				ArrayList<Integer> sub = new ArrayList<Integer>(list.subList(commands[i][0]-1, commands[i][1]));
+				Collections.sort(sub);
+				ret[i] = sub.get(commands[i][2]-1);
+			}
+			return ret;
+		}
+		
+	}
+
+
+//	===================== ETC functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+//-------------IO_Manager--------------
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
