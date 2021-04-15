c232a8e9fae7c92d1a96b8bac6f4d489062ffbd5
MarcelSin <sys920525@gmail.com>
2021-04-15 19:20:34 +0900

    Solve Lv.2 Make a BigNumber in Greedy

---
+++ Programmers_Lv2_MakeABigNumber/Main.java
@@ -0,0 +1,118 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
+	public Solution solve = new Solution();
+
+	public static void main(String[] args) throws IOException {
+		Execute();
+	}
+	
+	
+	static void Execute() {
+		Main mainObj = new Main();
+		String ans = mainObj.solve.solution("4177252841",4);
+		//Display(ans, ans.length);
+		System.out.println(ans);
+	}
+	
+	//import java.util.*;
+	class Solution {
+		int[][] DIST = new int[20][20];
+		
+		public String solution(String number, int k) {
+			StringBuilder sb = new StringBuilder();
+			char[] nums = number.toCharArray();
+			int maxIdx=-1,len = number.length(),left,right;
+			k = len-k;
+			while(0 < k) {
+				left = maxIdx+1;
+				right = len-k;
+				maxIdx = left;
+				for (int i = left+1; i <= right; i++) {
+					if(nums[i] > nums[maxIdx]) maxIdx = i;
+				}
+				sb.append(nums[maxIdx]);
+				k--;
+			}
+			return sb.toString();
+		}
+			
+	}
+
+
+//	===================== ETC functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+//-------------IO_Manager--------------
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
