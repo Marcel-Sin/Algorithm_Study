2de199cd92f6432e9a49c9891ad58c0422b46921
MarcelSin <sys920525@gmail.com>
2021-04-26 11:19:43 +0900

    Solve Lv.3 Migrant_Judge in BinarySearch

---
+++ Programmers_Lv3_migrantJudge/Main.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+	static IO_Manager io = new IO_Manager();
+	static final int NINF = Integer.MIN_VALUE / 2;
+	static final int INF = Integer.MAX_VALUE / 2;
+	static final int MAX = 1000;
+	
+	public Solution solve = new Solution();
+
+	public static void main(String[] args) throws IOException {
+		Execute();
+	}
+	
+	
+	static void Execute() {
+		Main mainObj = new Main();
+		long ans = mainObj.solve.solution(10,new int[] {3,7,10});
+		//Display(ans, ans.length);
+		System.out.println(ans);
+	}
+	
+	// import java.util.*;
+	class Solution {
+	    public long solution(int n, int[] times) {
+	        long answer = 0;
+	        Arrays.sort(times);
+	        long low=0,high=((long)n*(long)times[times.length-1]),mid;
+	        long minValue=Long.MAX_VALUE,count;
+	        while(low < high) {
+	      	  mid = (low+high)/2;
+	      	  count = Max_Processing(mid, times);
+	      	  if(n <= count) {
+	      		  minValue = (minValue > mid) ? mid:minValue;
+	      		  high = mid;
+	      	  }
+	      	  else low = mid+1;
+	        }
+	        return minValue;
+	    }
+	    long Max_Processing(long time,int[] judges) {
+	   	 long ret = 0;
+	   	 for (int i = 0; i < judges.length; i++) {
+				ret += time/(long)judges[i];
+			}
+	   	 return ret;
+	    }
+	}
+
+//	===================== ETC functions for PS =====================
+	static int nextInt(StringTokenizer stk) {
+		return Integer.parseInt(stk.nextToken());
+	}
+	static long Min(long a, long b) {
+		return (a > b) ? b : a;
+	}
+	static long Max(long a, long b) {
+		return (a > b) ? a : b;
+	}
+	static int Min(int a, int b) {
+		return (a > b) ? b : a;
+	}
+	static int Max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	static double Min(double a, double b) {
+		return (a > b) ? b : a;
+	}
+	static double Max(double a, double b) {
+		return (a > b) ? a : b;
+	}
+	static void Display(int[] arr, int limit) {
+		for (int i = 0; i < limit; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	static void Display(int[][] arr, int limit) {
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < limit; j++) {
+				System.out.printf(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+//-------------IO_Manager--------------
+class IO_Manager {
+	public BufferedReader br;
+	public BufferedWriter bw;
+
+	public IO_Manager() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	public Integer inputInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+	public Long inputLong() throws IOException {
+		return Long.parseLong(br.readLine());
+	}
+	public String inputStr() throws IOException {
+		return br.readLine();
+	}
+	public void write(String str) throws IOException {
+		bw.write(str);
+	}
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
